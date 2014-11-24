ce782904618bcd5254a0ee00765f25e65d8e0013
michaelt919 <4734191+michaelt919@users.noreply.github.com>
2014-11-24 15:13:33 -0600

    Added a couple of files, testing Mercurial Eclipse

---
+++ Java/ReflectanceAcquisition/src/openGL/wrappers/implementations/OpenGLTexture.java
@@ -0,0 +1,12 @@
+package openGL.wrappers.implementations;
+
+import openGL.wrappers.interfaces.Texture;
+
+public class OpenGLTexture implements Texture 
+{
+
+	public OpenGLTexture() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
