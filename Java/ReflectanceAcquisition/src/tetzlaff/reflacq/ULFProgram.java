0e0edda9d784a4a8008a197240b1c1a541a67880
michaelt919 <4734191+michaelt919@users.noreply.github.com>
2015-01-09 11:12:30 -0600

    Split MainProgram into two programs, one for ULF rendering alone, and the other for texture generation

---
+++ Java/ReflectanceAcquisition/src/tetzlaff/reflacq/ULFProgram.java
@@ -0,0 +1,30 @@
+package tetzlaff.reflacq;
+
+import tetzlaff.gl.helpers.InteractiveGraphics;
+import tetzlaff.gl.helpers.Trackball;
+import tetzlaff.interactive.InteractiveApplication;
+import tetzlaff.ulf.ULFRendererList;
+import tetzlaff.window.glfw.GLFWWindow;
+
+public class ULFProgram
+{
+    public static void main(String[] args) 
+    {
+    	GLFWWindow window = new GLFWWindow(800, 800, "Unstructured Light Field Renderer", true, 4);
+    	window.enableDepthTest();
+    	
+    	Trackball trackball = new Trackball(1.0f);
+        trackball.addAsWindowListener(window);
+        
+        ULFRendererList model = new ULFRendererList(window, trackball);
+    	ULFUserInterface gui = new ULFUserInterface(model);
+        
+        InteractiveApplication app = InteractiveGraphics.createApplication(window, window, model.getDrawable());
+        window.show();
+        gui.show();
+		app.run();
+        
+        GLFWWindow.closeAllWindows();
+        System.exit(0);
+    }
+}
