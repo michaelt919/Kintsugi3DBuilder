f1d2415b2694d1f7503080e0d896ea276ec892cc
michaelt919 <michaelt919@localhost>
2015-01-13 00:12:08 -0600

    Checkpoint on model fitting.

--- Java/ReflectanceAcquisition/shaders/ulfTex.frag
+++ Java/ReflectanceAcquisition/shaders/ulfTex.frag
@@ -4,29 +4,21 @@
 #define MAX_CAMERA_PROJECTION_COUNT 256
 
 uniform bool occlusionEnabled;
-
-uniform float weightExponent;
 uniform float occlusionBias;
-uniform float gamma;
-
-uniform mat4 model_view;
 
 in vec3 fPosition;
 in vec2 fTexCoord;
 in vec3 fNormal;
 
-in vec3 fViewPos;
-
 uniform sampler2DArray imageTextures;
 uniform sampler2DArray depthTextures;
-uniform sampler2D testTexture;
 
 uniform CameraPoses
 {
 	mat4 cameraPoses[MAX_CAMERA_POSE_COUNT];
 };
 
-uniform int cameraPoseCount;
+uniform int cameraPoseIndex;
 
 uniform CameraProjections
 {
@@ -38,56 +30,32 @@ uniform CameraProjectionIndices
 	int cameraProjectionIndices[MAX_CAMERA_POSE_COUNT];
 };
 
-uniform mat4 cameraProj;
-
 layout(location = 0) out vec4 fragColor;
 
-float computeSampleWeight(vec3 cameraPos, vec3 samplePos, vec3 fragmentPos)
+void main()
 {
-	return 1.0 / (1.0 - pow(max(0.0, dot(normalize(samplePos - fragmentPos), 
-		normalize(cameraPos - fragmentPos))), weightExponent)) - 1.0;
-}
-
-vec4 getLightFieldSample(int index)
-{
-	vec4 fragPos = cameraPoses[index] * vec4(fPosition, 1.0);
-	vec4 projPos = cameraProjections[cameraProjectionIndices[index]] * fragPos;
+	vec4 fragPos = cameraPoses[cameraPoseIndex] * vec4(fPosition, 1.0);
+	vec4 projPos = cameraProjections[cameraProjectionIndices[cameraPoseIndex]] * fragPos;
 	projPos = projPos / projPos.w;
 	
 	vec2 texCoord = vec2(projPos.x / 2 + 0.5, projPos.y / 2 + 0.5);
 	
 	if (texCoord.x < 0 || texCoord.x > 1 || texCoord.y < 0 || texCoord.y > 1)
 	{
-		return vec4(0.0);
+		fragColor = vec4(0.0);
 	}
 	else
 	{
 		if (occlusionEnabled)
 		{
-			float imageDepth = 2*texture(depthTextures, vec3(texCoord.xy, index)).x - 1;
+			float imageDepth = 2*texture(depthTextures, vec3(texCoord.xy, cameraPoseIndex)).x - 1;
 			if (abs(projPos.z - imageDepth) > occlusionBias)
 			{
 				// Occluded
-				return vec4(0.0);
+				discard;
 			}
 		}
 		
-		return computeSampleWeight((cameraPoses[index] * vec4(fViewPos, 1.0)).xyz, vec3(0.0), fragPos.xyz)
-			* pow(texture(imageTextures, vec3(texCoord.xy, index)), vec4(gamma));
+		fragColor = texture(imageTextures, vec3(texCoord.xy, cameraPoseIndex));
 	}
-}
-
-vec4 computeLightField()
-{
-	vec4 sum = vec4(0.0);
-	for (int i = 0; i < cameraPoseCount; i++)
-	{
-		sum += getLightFieldSample(i);
-	}
-	return pow(sum / sum.w, vec4(1 / gamma));
-}
-
-void main()
-{
-	fragColor = computeLightField();
 }
--- Java/ReflectanceAcquisition/src/tetzlaff/reflacq/TexGenProgram.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/reflacq/TexGenProgram.java
@@ -1,58 +1,349 @@
 package tetzlaff.reflacq;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 
-import tetzlaff.gl.helpers.InteractiveGraphics;
-import tetzlaff.gl.helpers.Trackball;
-import tetzlaff.interactive.InteractiveApplication;
-import tetzlaff.ulf.ULFRendererList;
-import tetzlaff.ulf.ULFToTexturesList;
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.apache.commons.math3.fitting.GaussianCurveFitter;
+import org.apache.commons.math3.fitting.WeightedObservedPoints;
+import org.apache.commons.math3.special.Erf;
+
+import tetzlaff.gl.PrimitiveMode;
+import tetzlaff.gl.opengl.OpenGLContext;
+import tetzlaff.gl.opengl.OpenGLFramebufferObject;
+import tetzlaff.gl.opengl.OpenGLProgram;
+import tetzlaff.gl.opengl.OpenGLRenderable;
+import tetzlaff.gl.opengl.OpenGLResource;
+import tetzlaff.ulf.UnstructuredLightField;
 import tetzlaff.window.glfw.GLFWWindow;
 
 public class TexGenProgram
 {
+	private static final double PI_OVER_2_SQRT_2 = 2 * Math.sqrt(2);
+	private static final double SQRT_2_PI = Math.sqrt(2 * Math.PI);
+	
+	private static final double MIN_SIGMA = 1.0 / 256.0;
+	
+	private static double getSpecularIntensity(double incomingIntensity, double incomingSigma, double outgoingIntensity, double outgoingSigma, double specularRoughness)
+	{
+		if (incomingIntensity <= 0.0 || incomingSigma <= 0.0 || outgoingIntensity <= 0.0 || outgoingSigma <= 0.0 || specularRoughness <= 0.0)
+		{
+			return 0.0;
+		}
+		else
+		{
+			double incomingSigmaTimesRoughness = incomingSigma * specularRoughness;
+			return outgoingIntensity * outgoingSigma / (incomingIntensity * incomingSigmaTimesRoughness * SQRT_2_PI * Erf.erf(outgoingSigma * PI_OVER_2_SQRT_2 / incomingSigmaTimesRoughness));
+		}
+	}
+	
+	private static double getSpecularRoughness(double incomingSigma, double outgoingSigma)
+	{
+		if (outgoingSigma <= incomingSigma)
+		{
+			return MIN_SIGMA;
+		}
+		else
+		{
+			return Math.sqrt(outgoingSigma*outgoingSigma - incomingSigma*incomingSigma);
+		}
+	}
+	
     public static void main(String[] args) 
     {
-    	GLFWWindow ulrWindow = new GLFWWindow(800, 800, "Unstructured Light Field Renderer", 280, 140, true, 4);
-    	ulrWindow.enableDepthTest();
-    	ulrWindow.enableBackFaceCulling();
-    	
-    	GLFWWindow ulfToTexWindow = new GLFWWindow(800, 800, "Texture Space Visualization", 1100, 140, true, 4);
-    	ulfToTexWindow.enableDepthTest();
-    	ulfToTexWindow.enableBackFaceCulling();
-    	
-    	Trackball trackball = new Trackball(1.0f);
-        trackball.addAsWindowListener(ulrWindow);
-        
-    	ULFRendererList model = new ULFRendererList(ulrWindow, trackball);
-    	ULFToTexturesList ulfToTexList = new ULFToTexturesList(ulfToTexWindow, trackball);
-    	ULFUserInterface gui = new ULFUserInterface(model);
-    	
-        gui.addSelectedLightFieldListener(lf -> 
-        {
-        	ulfToTexList.setSelectedItem(lf);
-        	if (ulfToTexList.getSelectedItem() == null)
-        	{
-        		try 
-        		{
-					ulfToTexList.addFromDirectory(lf.directoryPath);
-				} 
-        		catch (Exception e)
-        		{
-					e.printStackTrace();
+    	JFileChooser fileChooser = new JFileChooser(new File("").getAbsolutePath());
+		fileChooser.removeChoosableFileFilter(fileChooser.getAcceptAllFileFilter());
+		fileChooser.setFileFilter(new FileNameExtensionFilter("View Set files (.vset)", "vset"));
+		
+		if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+		{
+			OpenGLContext ulfToTexContext = new GLFWWindow(800, 800, "Texture Generation");
+	    	ulfToTexContext.enableDepthTest();
+	    	ulfToTexContext.enableBackFaceCulling();
+	    	
+	    	String lightFieldDirectory = fileChooser.getSelectedFile().getParent();
+	    	int textureSize = 1024;
+	    	
+	    	int maxFittingIterations = 100;
+	    	
+	    	double lightIntensityRed = 1.0;
+	    	double lightIntensityGreen = 1.0;
+	    	double lightIntensityBlue = 1.0;
+	    	double lightSigmaRed = 0.5;
+	    	double lightSigmaGreen = 0.5;
+	    	double lightSigmaBlue = 0.5;
+	        
+	        try
+	        {
+	    		OpenGLProgram genTexProgram = new OpenGLProgram(new File("shaders\\ulfTex.vert"), new File("shaders\\ulfTex.frag"));
+	    		UnstructuredLightField lightField = UnstructuredLightField.loadFromDirectory(lightFieldDirectory);
+		    	
+		    	OpenGLRenderable renderable = new OpenGLRenderable(genTexProgram);
+		    	Iterable<OpenGLResource> vboResources = renderable.addVertexMesh("position", "texCoord", "normal", lightField.proxy);
+		    	
+		    	renderable.program().setTexture("imageTextures", lightField.viewSet.getTextures());
+		    	renderable.program().setUniformBuffer("CameraPoses", lightField.viewSet.getCameraPoseBuffer());
+		    	renderable.program().setUniformBuffer("CameraProjections", lightField.viewSet.getCameraProjectionBuffer());
+		    	renderable.program().setUniformBuffer("CameraProjectionIndices", lightField.viewSet.getCameraProjectionIndexBuffer());
+				
+		    	renderable.program().setTexture("depthTextures", lightField.depthTextures);
+		    	
+		    	renderable.program().setUniform("occlusionEnabled", lightField.settings.isOcclusionEnabled());
+		    	renderable.program().setUniform("occlusionBias", lightField.settings.getOcclusionBias());
+		    	
+		    	OpenGLFramebufferObject framebuffer = new OpenGLFramebufferObject(textureSize, textureSize, 1, false);
+		    	
+		    	System.out.println("Projecting light field images into texture space...");
+		    	Date timestamp = new Date();
+		    	
+		    	new File(lightFieldDirectory + "\\output\\textures").mkdirs();
+		    	
+		    	int[][] viewTextureData = new int[lightField.viewSet.getCameraPoseCount()][];
+		    	
+		    	for (int i = 0; i < lightField.viewSet.getCameraPoseCount(); i++)
+		    	{
+			    	framebuffer.clearColorBuffer(0, 0.0f, 0.0f, 0.0f, 0.0f);
+			    	framebuffer.clearDepthBuffer();
+			    	
+			    	renderable.program().setUniform("cameraPoseIndex", i);
+			        renderable.draw(PrimitiveMode.TRIANGLES, framebuffer);
+			        
+			        viewTextureData[i] = framebuffer.readColorBufferARGB(0);
+		    	}
+		        
+		        for (OpenGLResource r : vboResources)
+		    	{
+		    		r.delete();
+		    	}
+
+		    	framebuffer.delete();
+		        lightField.deleteOpenGLResources();
+		        genTexProgram.delete();
+		    	
+				System.out.println("Projections completed in " + (new Date().getTime() - timestamp.getTime()) + " milliseconds.");
+				
+				System.out.println("Sorting texels by intensity...");
+		    	timestamp = new Date();
+		    	
+				int sampleCount = viewTextureData.length;
+				int texelCount = textureSize*textureSize;
+				// Use shorts since Java doesn't support unsigned bytes
+		    	Short[][] redTexels = new Short[texelCount][sampleCount];
+		    	Short[][] greenTexels = new Short[texelCount][sampleCount];
+		    	Short[][] blueTexels = new Short[texelCount][sampleCount];
+		    	int[] alphaSums = new int[texelCount];
+		    	
+		    	for (int i = 0; i < sampleCount; i++)
+		    	{
+		    		// Flip the data vertically
+			        for (int y = 0; y < textureSize / 2; y++)
+			        {
+			        	int limit = (y + 1) * textureSize;
+			        	for (int j1 = y * textureSize, j2 = (textureSize - y - 1) * textureSize; j1 < limit; j1++, j2++)
+			        	{
+			        		alphaSums[j1] += ((viewTextureData[i][j2] & 0xFF000000) >>> 24);
+			    			redTexels[j1][i] = (short)((viewTextureData[i][j2] & 0x00FF0000) >>> 16);
+			    			greenTexels[j1][i] = (short)((viewTextureData[i][j2] & 0x0000FF00) >>> 8);
+			    			blueTexels[j1][i] = (short)(viewTextureData[i][j2] & 0x000000FF);
+			    			
+			    			alphaSums[j2] += ((viewTextureData[i][j1] & 0xFF000000) >>> 24);
+			    			redTexels[j2][i] = (short)((viewTextureData[i][j1] & 0x00FF0000) >>> 16);
+			    			greenTexels[j2][i] = (short)((viewTextureData[i][j1] & 0x0000FF00) >>> 8);
+			    			blueTexels[j2][i] = (short)(viewTextureData[i][j1] & 0x000000FF);
+			        	}
+			        }
+		    	}
+
+		        BufferedImage outImg = new BufferedImage(textureSize, textureSize, BufferedImage.TYPE_INT_ARGB);
+		        for (int y = 0, i = 0; y < textureSize; y++)
+		        {
+		        	for (int x = 0; x < textureSize; x++, i++)
+		        	{
+		        		outImg.setRGB(x, y, 0xFF800000 | Math.min(alphaSums[i] / sampleCount, 0x000000FF) << 8);
+		        	}
+		        }
+		        File outputFile = new File(lightFieldDirectory + String.format("\\output\\sums.png"));
+		        ImageIO.write(outImg, "PNG", outputFile);
+		    	
+		    	for (int i = 0; i < texelCount; i++)
+		    	{
+			    	Collections.sort(Arrays.asList(redTexels[i]));
+			    	Collections.sort(Arrays.asList(greenTexels[i]));
+			    	Collections.sort(Arrays.asList(blueTexels[i]));
+		    	}
+
+				System.out.println("Sorting completed in " + (new Date().getTime() - timestamp.getTime()) + " milliseconds.");
+				
+				System.out.println("Fitting model...");
+		    	timestamp = new Date();
+				
+				int[] diffuseImgData = new int[texelCount];
+				int[] specularIntensityData = new int[texelCount];
+				int[] specularRoughnessData = new int[texelCount];
+				int[] specularOffsetData = new int[texelCount];
+				
+				for (int texelIndex = 0; texelIndex < texelCount; texelIndex++)
+				{
+					if (texelCount > 100 && texelIndex % (texelCount / 100) == 0)
+					{
+						System.out.println(texelIndex / (texelCount / 100) + "% complete.");
+					}
+					
+					int diffuseIndex = texelCount / 2;
+					while (diffuseIndex < texelCount - 1 && diffuseImgData[texelIndex] == 0x00000000)
+					{
+						diffuseIndex++;
+					}
+						
+	        		short diffuseRed = redTexels[texelIndex][diffuseIndex];
+	        		short diffuseGreen = greenTexels[texelIndex][diffuseIndex];
+	        		short diffuseBlue = blueTexels[texelIndex][diffuseIndex];
+					diffuseImgData[texelIndex] = 0xFF000000 | diffuseRed << 16 | diffuseGreen << 8 | diffuseBlue;
+					
+					if (sampleCount - diffuseIndex > 3)
+					{
+						GaussianCurveFitter redFitter = GaussianCurveFitter.create()
+							.withMaxIterations(maxFittingIterations);
+						
+						GaussianCurveFitter greenFitter = GaussianCurveFitter.create()
+							.withMaxIterations(maxFittingIterations);
+
+						GaussianCurveFitter blueFitter = GaussianCurveFitter.create()
+							.withMaxIterations(maxFittingIterations);
+	
+				        WeightedObservedPoints redPoints = new WeightedObservedPoints();
+				        WeightedObservedPoints greenPoints = new WeightedObservedPoints();
+				        WeightedObservedPoints bluePoints = new WeightedObservedPoints();
+	
+				        for (int sampleIndex = diffuseIndex + 1; sampleIndex < sampleCount; sampleIndex++) 
+				        {
+				        	// Assume diffuse index is at an angle of pi/2 (between dominant light direction and viewing direction)
+				        	// theta is measured in radians / (pi/2) - i.e. theta=1.0 is a 90 degree angle, theta = 0.0 is a 0 degree angle
+				        	double theta = (double)(sampleCount - sampleIndex - 0.5) / (double)(sampleCount - diffuseIndex - 0.5);
+				        	
+				        	short red = redTexels[texelIndex][sampleIndex];
+				        	if (red < 255)
+				        	{
+				        		// Add sample points at theta and negative theta to ensure that the Gaussian is centered
+				        		redPoints.add(theta, red - diffuseRed);
+				        		redPoints.add(-theta, red - diffuseRed);
+				        	}
+	
+			        		short green = greenTexels[texelIndex][sampleIndex];
+				        	if (green < 255)
+				        	{
+				        		greenPoints.add(theta, green - diffuseGreen);
+				        		greenPoints.add(-theta, green - diffuseGreen);
+				        	}
+				        	
+				        	short blue = blueTexels[texelIndex][sampleIndex];
+				        	if (blue < 255)
+				        	{
+				        		bluePoints.add(theta, blue - diffuseBlue);
+				        		bluePoints.add(-theta, blue - diffuseBlue);
+				        	}
+				        }
+	
+				        try
+				        {
+					        double[] redFit = redFitter.fit(redPoints.toList());
+					        double redNorm = redFit[0];
+					        double redCenter = redFit[1]; // Should be zero
+					        double redSigma = redFit[2];
+					        double redRoughness = getSpecularRoughness(lightSigmaRed, redSigma);
+					        double redIntensity = getSpecularIntensity(lightIntensityRed, lightSigmaRed, redNorm, redSigma, redRoughness);
+					        short redIntensityInteger = (short)Math.min(Math.round(redIntensity), 255);
+					        short redRoughnessInteger = (short)Math.min(Math.max(Math.round(redRoughness*256 - 1), 0), 255);
+					        short redOffsetInteger = (short)Math.min(Math.round(redCenter*255), 255);
+
+					        double[] greenFit = greenFitter.fit(greenPoints.toList());
+					        double greenNorm = greenFit[0];
+					        double greenCenter = greenFit[1]; // Should be zero
+					        double greenSigma = greenFit[2];
+					        double greenRoughness = getSpecularRoughness(lightSigmaGreen, greenSigma);
+					        double greenIntensity = getSpecularIntensity(lightIntensityGreen, lightSigmaGreen, greenNorm, greenSigma, greenRoughness);
+					        short greenIntensityInteger = (short)Math.min(Math.round(greenIntensity), 255);
+					        short greenRoughnessInteger = (short)Math.min(Math.max(Math.round(greenRoughness*256 - 1), 0), 255);
+					        short greenOffsetInteger = (short)Math.min(Math.round(greenCenter*255), 255);
+					        
+					        double[] blueFit = blueFitter.fit(bluePoints.toList());
+					        double blueNorm = blueFit[0];
+					        double blueCenter = blueFit[1]; // Should be zero
+					        double blueSigma = blueFit[2];
+					        double blueRoughness = getSpecularRoughness(lightSigmaBlue, blueSigma);
+					        double blueIntensity = getSpecularIntensity(lightIntensityBlue, lightSigmaBlue, blueNorm, blueSigma, blueRoughness);
+					        short blueIntensityInteger = (short)Math.min(Math.round(blueIntensity), 255);
+					        short blueRoughnessInteger = (short)Math.min(Math.max(Math.round(blueRoughness*256 - 1), 0), 255);
+					        short blueOffsetInteger = (short)Math.min(Math.round(blueCenter*255), 255);
+				        
+					        specularIntensityData[texelIndex] = 0xFF000000 | redIntensityInteger << 16 | greenIntensityInteger << 8 | blueIntensityInteger;
+					        specularRoughnessData[texelIndex] = 0xFF000000 | redRoughnessInteger << 16 | greenRoughnessInteger << 8 | blueRoughnessInteger;
+					        specularOffsetData[texelIndex] = 0xFF000000 | redOffsetInteger << 16 | greenOffsetInteger << 8 | blueOffsetInteger;
+				        }
+
+				        catch(Exception e)
+				        {
+				        	e.printStackTrace();
+				        }
+					}
 				}
-        	}
-        });
-        
-        InteractiveApplication ulrApp = InteractiveGraphics.createApplication(ulrWindow, ulrWindow, model.getDrawable());
-        InteractiveApplication ulfToTexApp = InteractiveGraphics.createApplication(ulfToTexWindow, ulfToTexWindow, ulfToTexList.getDrawable());
-        
-        gui.show();
-        ulrWindow.show();
-        ulfToTexWindow.show();
-		InteractiveApplication.runSimultaneous(Arrays.asList(ulrApp, ulfToTexApp));
-        
-        GLFWWindow.closeAllWindows();
-        System.exit(0);
+				
+				System.out.println("Model fitting completed in " + (new Date().getTime() - timestamp.getTime()) + " milliseconds.");
+
+		    	System.out.println("Writing to output directory: " + lightFieldDirectory + "\\output");
+				timestamp = new Date();
+		    	
+		    	outImg = new BufferedImage(textureSize, textureSize, BufferedImage.TYPE_INT_ARGB);
+		        outImg.setRGB(0, 0, textureSize, textureSize, diffuseImgData, 0, textureSize);
+		        outputFile = new File(lightFieldDirectory + String.format("\\output\\textures\\diffuse.png"));
+		        ImageIO.write(outImg, "PNG", outputFile);
+		    	
+		    	outImg = new BufferedImage(textureSize, textureSize, BufferedImage.TYPE_INT_ARGB);
+		        outImg.setRGB(0, 0, textureSize, textureSize, specularIntensityData, 0, textureSize);
+		        outputFile = new File(lightFieldDirectory + String.format("\\output\\textures\\specularIntensity.png"));
+		        ImageIO.write(outImg, "PNG", outputFile);
+		    	
+		    	outImg = new BufferedImage(textureSize, textureSize, BufferedImage.TYPE_INT_ARGB);
+		        outImg.setRGB(0, 0, textureSize, textureSize, specularRoughnessData, 0, textureSize);
+		        outputFile = new File(lightFieldDirectory + String.format("\\output\\textures\\specularRoughness.png"));
+		        ImageIO.write(outImg, "PNG", outputFile);
+		    	
+		    	outImg = new BufferedImage(textureSize, textureSize, BufferedImage.TYPE_INT_ARGB);
+		        outImg.setRGB(0, 0, textureSize, textureSize, specularOffsetData, 0, textureSize);
+		        outputFile = new File(lightFieldDirectory + String.format("\\output\\textures\\specularOffset.png"));
+		        ImageIO.write(outImg, "PNG", outputFile);
+				
+				for (int i = 0; i < sampleCount; i++)
+		    	{
+			        outImg = new BufferedImage(textureSize, textureSize, BufferedImage.TYPE_INT_ARGB);
+			        for (int y = 0, j = 0; y < textureSize; y++)
+			        {
+			        	for (int x = 0; x < textureSize; x++, j++)
+			        	{
+			        		outImg.setRGB(x, y, 0xFF000000 | (redTexels[j][i] << 16) | (greenTexels[j][i] << 8) | blueTexels[j][i]);
+			        	}
+			        }
+			        
+			        outputFile = new File(lightFieldDirectory + String.format("\\output\\textures\\%1$04d.png", i));
+			        ImageIO.write(outImg, "PNG", outputFile);
+			        
+		    	}
+				System.out.println("Output files saved in " + (new Date().getTime() - timestamp.getTime()) + " milliseconds.");
+	        }
+	        catch (IOException e)
+	        {
+	        	e.printStackTrace();
+	        }
+	        
+	        GLFWWindow.closeAllWindows();
+	        System.exit(0);
+		}
     }
 }
