6b44e4c8215ddd56196ea280e83b895393603db0
michaelt919 <michaelt919@localhost>
2015-02-14 12:53:42 -0600

    Some miscellaneous changes

--- Java/ReflectanceAcquisition/shaders/modelfit.frag
+++ Java/ReflectanceAcquisition/shaders/modelfit.frag
@@ -11,39 +11,65 @@ uniform sampler2DArray textures;
 uniform int textureCount;
 uniform float gamma;
 
+uniform float guessSpecularWeight;
+uniform vec3 guessSpecularColor;
+
 uniform CameraPoses
 {
 	mat4 cameraPoses[MAX_CAMERA_POSE_COUNT];
 };
 
-layout(location = 0) out vec4 ambientEstimate;
-layout(location = 1) out vec4 diffuseEstimate;
-layout(location = 2) out vec4 normalEstimate;
+layout(location = 0) out vec4 diffuseColor;
+layout(location = 1) out vec4 normalMap;
+layout(location = 2) out vec4 specularColor;
+layout(location = 3) out vec4 specularRoughness;
+layout(location = 4) out vec4 ambientColor;
+
+vec4 getColor(int index)
+{
+    return pow(texture(textures, vec3(fTexCoord, index)), vec4(gamma));
+}
+
+vec3 getViewVector(int index)
+{
+    return normalize(transpose(mat3(cameraPoses[index])) * -cameraPoses[index][3].xyz - fPosition);
+}
+
+vec3 getLightVector(int index)
+{
+    // TODO
+    return getViewVector(index);
+}
+
+vec3 getReflectionVector(vec3 normalVector, vec3 lightVector)
+{
+    return 2 * dot(lightVector, normalVector) * normalVector - lightVector;
+}
 
 void main()
 {
 	vec4 sumColor = vec4(0);
     for (int i = 0; i < textureCount; i++)
     {   
-       vec4 color = pow(texture(textures, vec3(fTexCoord, i)), vec4(gamma));
+       vec4 color = getColor(i);
        sumColor += color.a * vec4(color.rgb, 1.0);
     }
     float avgIntensity = (sumColor.r + sumColor.g + sumColor.b) / sumColor.a;
     
-    mat4 a = mat4(0);
-    mat4 b = mat4(0);
+    mat4 dA = mat4(0);
+    mat4 dB = mat4(0);
     
     vec4 diffuseSum = vec4(0);
     for (int i = 0; i < textureCount; i++)
     {
-        vec4 color = pow(texture(textures, vec3(fTexCoord, i)), vec4(gamma));
+        vec4 color = getColor(i);
         if (color.a > 0)
         {
-            vec4 camera = vec4(normalize(transpose(mat3(cameraPoses[i])) * -cameraPoses[i][3].xyz - fPosition), 1.0);
-            vec4 light = camera; // TODO
+            vec4 view = vec4(getViewVector(i), 1.0);
+            vec4 light = vec4(getLightVector(i), 1.0);
             
-            a += color.a * outerProduct(light, light);
-            b += color.a * outerProduct(light, vec4(color.rgb, 0.0));
+            dA += color.a * outerProduct(light, light);
+            dB += color.a * outerProduct(light, vec4(color.rgb, 0.0));
             
             if ((color.r > 0.0 || color.g > 0.0 || color.b > 0.0) && 
                 (color.r + color.g + color.b) < avgIntensity)
@@ -52,12 +78,74 @@ void main()
             }
         }
     }
-    
-    vec4 c = inverse(a) * b * vec4(diffuseSum.rgb / max(max(diffuseSum.r, diffuseSum.g), diffuseSum.b), 0.0);
-    float ambientIntensity = c.w;
-    float diffuseIntensity = length(c.xyz);
-    normalEstimate = vec4(c.xyz / diffuseIntensity * 0.5 + vec3(0.5), 1.0);
     vec3 diffuseAvg = diffuseSum.rgb / (diffuseSum.r + diffuseSum.g + diffuseSum.b);
-    ambientEstimate = pow(vec4(diffuseAvg * ambientIntensity, 1.0), vec4(1 / gamma));
-    diffuseEstimate = pow(vec4(diffuseAvg * diffuseIntensity, 1.0), vec4(1 / gamma));
+    
+    vec3 simpleWeights = diffuseSum.rgb / max(max(diffuseSum.r, diffuseSum.g), diffuseSum.b);
+    vec3 rgbWeights;
+    if (guessSpecularWeight > 0)
+    {
+        vec3 ortho = cross(diffuseAvg, guessSpecularColor);
+        if (ortho.x > 0 || ortho.y > 0 || ortho.z > 0)
+        {
+            mat3 colorBasis = mat3(diffuseAvg, guessSpecularColor, ortho);
+            float adjustedFixedSpecWeight = guessSpecularWeight * length(ortho) / 
+                (length(diffuseAvg) * length(guessSpecularColor));
+            rgbWeights = (adjustedFixedSpecWeight * transpose(inverse(colorBasis))[0] + simpleWeights) / 
+                (1 + adjustedFixedSpecWeight);
+        }
+        else
+        {
+            rgbWeights = simpleWeights;
+        }
+    }
+    else
+    {
+        rgbWeights = simpleWeights;
+    }
+    
+    vec4 dSolution = inverse(dA) * dB * vec4(rgbWeights, 0.0);
+    float ambientIntensity = dSolution.w;
+    float diffuseIntensity = length(dSolution.xyz);
+    vec3 normal = dSolution.xyz / diffuseIntensity;
+    normalMap = vec4(normal * 0.5 + vec3(0.5), 1.0);
+    ambientColor = vec4(pow(diffuseAvg * dSolution.w, vec3(1 / gamma)), 1.0);
+    diffuseColor = vec4(pow(diffuseAvg * diffuseIntensity, vec3(1 / gamma)), 1.0);
+    
+    vec4 specularSum = vec4(0);
+    mat2 sA = mat2(0);
+    vec2 sB = vec2(0);
+    
+    for (int i = 0; i < textureCount; i++)
+    {
+        vec4 color = getColor(i);
+        if (color.a > 0)
+        {
+            vec3 view = getViewVector(i);
+            vec3 light = getLightVector(i);
+            
+            vec3 diffuseContrib = diffuseColor.rgb * max(0, dot(light, normal));
+            vec4 colorRemainder = vec4(max(vec3(0), color.rgb - diffuseContrib), color.a);
+            float intensity = colorRemainder.r + colorRemainder.g + colorRemainder.b;
+            
+            if (intensity > 0.0)
+            {
+                vec3 reflection = getReflectionVector(normal, light);
+                float rDotV = dot(reflection, view);
+                if (rDotV > 0.0)
+                {
+                    float x = rDotV - 1 / rDotV;
+                    //sA += color.a * intensity / log(intensity) * outerProduct(vec2(x, 1), vec2(x, 1));
+                    //sB += color.a * intensity * vec2(x, 1);
+                    sA += color.a * outerProduct(vec2(x, 1), vec2(x, 1));
+                    sB += color.a * log(intensity) * vec2(x, 1);
+                    specularSum += colorRemainder.a * vec4(colorRemainder.rgb, 1.0);
+                }
+            }
+        }
+    }
+    
+    vec2 sSolution = inverse(sA) * sB;
+    vec3 specularAvg = specularSum.rgb / (diffuseSum.r + diffuseSum.g + diffuseSum.b);
+    specularColor = vec4(pow(exp(sSolution[1]) * specularAvg, vec3(1 / gamma)), 1.0);
+    specularRoughness = vec4(vec3(inversesqrt(2*sSolution[0])), 1.0);
 }
--- Java/ReflectanceAcquisition/src/tetzlaff/reflacq/TexGenProgram.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/reflacq/TexGenProgram.java
@@ -31,14 +31,15 @@ public class TexGenProgram
     	ulfToTexContext.enableBackFaceCulling();
     	
     	int textureSize = 1024;
-    	float gamma = 2.2f;
+    	float gamma = 1.0f; // 2.2f;
     	Vector3 guessSpecularColor = new Vector3(1.0f, 1.0f, 1.0f);
-    	float guessSpecularWeight = 100.0f;
+    	float guessSpecularWeight = 10.0f;
     	
         try
         {
 	    	OpenGLProgram worldToTextureProgram = new OpenGLProgram(new File("shaders\\texspace.vert"), new File("shaders\\projtex.frag"));
     		OpenGLProgram modelFitProgram = new OpenGLProgram(new File("shaders\\texspace.vert"), new File("shaders\\modelfit.frag"));
+    		OpenGLProgram specularDebugProgram = new OpenGLProgram(new File("shaders\\texspace.vert"), new File("shaders\\speculardebug.frag"));
     		
     		JFileChooser fileChooser = new JFileChooser(new File("").getAbsolutePath());
     		fileChooser.removeChoosableFileFilter(fileChooser.getAcceptAllFileFilter());
@@ -68,6 +69,8 @@ public class TexGenProgram
 		    	worldToTextureRenderable.program().setUniform("occlusionEnabled", lightField.settings.isOcclusionEnabled());
 		    	worldToTextureRenderable.program().setUniform("occlusionBias", lightField.settings.getOcclusionBias());
 		    	
+		    	new File(lightFieldDirectory + "\\output\\debug\\diffuse").mkdirs();
+		    	
 		    	for (int i = 0; i < lightField.viewSet.getCameraPoseCount(); i++)
 		    	{
 		    		worldToTextureFBO.setColorAttachment(0, textures.getLayerAsFramebufferAttachment(i));
@@ -76,6 +79,8 @@ public class TexGenProgram
 		    		worldToTextureFBO.clearColorBuffer(0, 0.0f, 0.0f, 0.0f, 0.0f);
 		    		worldToTextureFBO.clearDepthBuffer();
 		    		worldToTextureRenderable.draw(PrimitiveMode.TRIANGLES, worldToTextureFBO);
+		    		
+		    		worldToTextureFBO.saveColorBufferToFile(0, "PNG", String.format(lightFieldDirectory + "\\output\\debug\\diffuse\\%04d.png", i));
 		    	}		    	
 		    	
 		    	worldToTextureFBO.delete();
@@ -99,7 +104,7 @@ public class TexGenProgram
 		    	renderable.program().setUniform("guessSpecularColor", guessSpecularColor);
 		    	renderable.program().setUniform("guessSpecularWeight", guessSpecularWeight);
 		    	
-		    	OpenGLFramebufferObject framebuffer = new OpenGLFramebufferObject(textureSize, textureSize, 5, false);
+		    	OpenGLFramebufferObject framebuffer = new OpenGLFramebufferObject(textureSize, textureSize, 8, false);
 		    	
 		    	new File(lightFieldDirectory + "\\output\\textures").mkdirs();
 		    	
@@ -108,17 +113,58 @@ public class TexGenProgram
 		    	framebuffer.clearColorBuffer(2, 0.0f, 0.0f, 0.0f, 0.0f);
 		    	framebuffer.clearColorBuffer(3, 0.0f, 0.0f, 0.0f, 0.0f);
 		    	framebuffer.clearColorBuffer(4, 0.0f, 0.0f, 0.0f, 0.0f);
+		    	framebuffer.clearColorBuffer(5, 0.0f, 0.0f, 0.0f, 0.0f);
+		    	framebuffer.clearColorBuffer(6, 0.0f, 0.0f, 0.0f, 0.0f);
+		    	framebuffer.clearColorBuffer(7, 0.0f, 0.0f, 0.0f, 0.0f);
 		    	framebuffer.clearDepthBuffer();
 		    	
 		        renderable.draw(PrimitiveMode.TRIANGLES, framebuffer);
 		        
-		        framebuffer.saveColorBufferToFile(0, "PNG", lightFieldDirectory + "\\output\\textures\\ambient.png");
-		        framebuffer.saveColorBufferToFile(1, "PNG", lightFieldDirectory + "\\output\\textures\\diffuse.png");
-		        framebuffer.saveColorBufferToFile(2, "PNG", lightFieldDirectory + "\\output\\textures\\normal.png");
-		        framebuffer.saveColorBufferToFile(3, "PNG", lightFieldDirectory + "\\output\\textures\\specular.png");
-		        framebuffer.saveColorBufferToFile(4, "PNG", lightFieldDirectory + "\\output\\textures\\roughness.png");
+		        framebuffer.saveColorBufferToFile(0, "PNG", lightFieldDirectory + "\\output\\textures\\diffuse.png");
+		        framebuffer.saveColorBufferToFile(1, "PNG", lightFieldDirectory + "\\output\\textures\\normal.png");
+		        framebuffer.saveColorBufferToFile(2, "PNG", lightFieldDirectory + "\\output\\textures\\specular.png");
+		        framebuffer.saveColorBufferToFile(3, "PNG", lightFieldDirectory + "\\output\\textures\\roughness.png");
+		        framebuffer.saveColorBufferToFile(4, "PNG", lightFieldDirectory + "\\output\\debug\\ambient.png");
+		        framebuffer.saveColorBufferToFile(5, "PNG", lightFieldDirectory + "\\output\\debug\\debug1.png");
+		        framebuffer.saveColorBufferToFile(6, "PNG", lightFieldDirectory + "\\output\\debug\\debug2.png");
+		        framebuffer.saveColorBufferToFile(7, "PNG", lightFieldDirectory + "\\output\\debug\\debug3.png");
 
 		    	System.out.println("Model fitting completed in " + (new Date().getTime() - timestamp.getTime()) + " milliseconds.");
+		    	
+		    	System.out.println("Generating specular debug images...");
+		    	timestamp = new Date();
+	    		
+	    		OpenGLTextureArray specularDebugTextures = new OpenGLTextureArray(textureSize, textureSize, lightField.viewSet.getCameraPoseCount(), true, false);
+		    	OpenGLFramebufferObject specularDebugFBO = new OpenGLFramebufferObject(textureSize, textureSize, 0, false);
+		    	OpenGLRenderable specularDebugRenderable = new OpenGLRenderable(specularDebugProgram);
+		    	
+		    	Iterable<OpenGLResource> specularDebugResources = specularDebugRenderable.addVertexMesh("position", "texCoord", "normal", lightField.proxy);
+		    	specularDebugRenderable.program().setTexture("textures", textures);
+		    	specularDebugRenderable.program().setTexture("diffuse", framebuffer.getColorAttachmentTexture(0));
+		    	specularDebugRenderable.program().setUniformBuffer("CameraPoses", lightField.viewSet.getCameraPoseBuffer());
+		    	specularDebugRenderable.program().setUniform("gamma", gamma);
+		    	
+		    	new File(lightFieldDirectory + "\\output\\debug\\specular").mkdirs();
+		    	
+		    	for (int i = 0; i < lightField.viewSet.getCameraPoseCount(); i++)
+		    	{
+		    		specularDebugFBO.setColorAttachment(0, specularDebugTextures.getLayerAsFramebufferAttachment(i));
+		    		specularDebugRenderable.program().setUniform("textureIndex", i);
+		    		
+		    		specularDebugFBO.clearColorBuffer(0, 0.0f, 0.0f, 0.0f, 0.0f);
+		    		specularDebugFBO.clearDepthBuffer();
+		    		specularDebugRenderable.draw(PrimitiveMode.TRIANGLES, specularDebugFBO);
+		    		
+		    		specularDebugFBO.saveColorBufferToFile(0, "PNG", String.format(lightFieldDirectory + "\\output\\debug\\specular\\%04d.png", i));
+		    	}		    	
+		    	
+		    	specularDebugFBO.delete();
+		    	for (OpenGLResource r : specularDebugResources)
+		    	{
+		    		r.delete();
+		    	}
+		    	
+				System.out.println("Specular debug images completed in " + (new Date().getTime() - timestamp.getTime()) + " milliseconds.");
 		        
 		        textures.delete();
 		    	framebuffer.delete();
