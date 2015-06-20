25a439a93bfbb81de9c9d387a8d3f4f1bd368b8b
michaelt919 <michaelt919@localhost>
2015-06-20 17:21:26 -0500

    Implemented a crude UI for the texture generation program.

--- Java/ReflectanceAcquisition/src/tetzlaff/reflacq/TexGenProgram.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/reflacq/TexGenProgram.java
@@ -1,53 +1,30 @@
 package tetzlaff.reflacq;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Date;
-
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
-import tetzlaff.gl.PrimitiveMode;
-import tetzlaff.gl.helpers.Matrix3;
-import tetzlaff.gl.helpers.Matrix4;
-import tetzlaff.gl.helpers.Vector2;
-import tetzlaff.gl.helpers.Vector3;
 import tetzlaff.gl.opengl.OpenGLContext;
-import tetzlaff.gl.opengl.OpenGLFramebufferObject;
-import tetzlaff.gl.opengl.OpenGLProgram;
-import tetzlaff.gl.opengl.OpenGLRenderable;
-import tetzlaff.gl.opengl.OpenGLResource;
-import tetzlaff.gl.opengl.OpenGLTexture2D;
-import tetzlaff.gl.opengl.OpenGLTextureArray;
-import tetzlaff.gl.opengl.OpenGLVertexBuffer;
-import tetzlaff.ulf.UnstructuredLightField;
 import tetzlaff.window.glfw.GLFWWindow;
 
 public class TexGenProgram
 {
 	public static void main(String[] args)
     {
-		OpenGLContext context = new GLFWWindow(800, 800, "Texture Generation");
+		TexGenUserInterface gui = new TexGenUserInterface();
     	
-        try
-        {
-    		JFileChooser fileChooser = new JFileChooser(new File("").getAbsolutePath());
-    		fileChooser.removeChoosableFileFilter(fileChooser.getAcceptAllFileFilter());
-    		fileChooser.setFileFilter(new FileNameExtensionFilter("View Set files (.vset)", "vset"));
-    		
-    		if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-    		{
-    			new TexGenExecutor(context, fileChooser.getSelectedFile(), new TexGenParameters()).execute();
-    		}
-        }
-        catch (IOException e)
-        {
-        	e.printStackTrace();
-        }
-        
-        GLFWWindow.closeAllWindows();
-        System.out.println("Process terminated with no errors.");
-        System.exit(0);
+		gui.addExecuteButtonActionListener(e -> 
+		{
+	        try
+	        {
+	        	OpenGLContext context = new GLFWWindow(800, 800, "Texture Generation");
+	    		new TexGenExecutor(context, gui.getSelectedFile(), gui.getParameters()).execute();
+		        GLFWWindow.closeAllWindows();
+		        System.out.println("Process terminated with no errors.");
+	        }
+	        catch (IOException ex)
+	        {
+	        	ex.printStackTrace();
+	        }
+		});
+		
+		gui.show();
 	}
 }
