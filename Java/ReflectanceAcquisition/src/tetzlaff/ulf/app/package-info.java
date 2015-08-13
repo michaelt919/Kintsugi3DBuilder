dd9e2c04779a51511eaed1889eb554deea087d43
Seth Berrier <berriers@uwstout.edu>
2015-08-13 08:18:40 -0500

    WIP: javadoc additions (adding some package info entries, then the main entry point and related paths)

---
+++ Java/ReflectanceAcquisition/src/tetzlaff/ulf/app/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides top level objects for viewing unstructured light fields as well
+ * as controlling their settings.
+ *
+ * @since 1.0
+ * @see tetzlaff.ulf
+ */
+package tetzlaff.ulf.app;
