5292366cce09550df5db882e8c781dacfc718a65
michaelt919 <michaelt919@localhost>
2015-08-26 20:09:53 -0500

    Apply linear filtering when rendering at half resolution

--- Java/ReflectanceAcquisition/src/tetzlaff/gl/builders/framebuffer/ColorAttachmentSpec.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/gl/builders/framebuffer/ColorAttachmentSpec.java
@@ -10,4 +10,25 @@ public class ColorAttachmentSpec extends AttachmentSpec
 	{
 		this.internalFormat = internalFormat;
 	}
+	
+	@Override
+	public ColorAttachmentSpec setMultisamples(int samples, boolean fixedSampleLocations)
+	{
+		super.setMultisamples(samples, fixedSampleLocations);
+		return this;
+	}
+
+	@Override
+	public ColorAttachmentSpec setMipmapsEnabled(boolean enabled)
+	{
+		super.setMipmapsEnabled(enabled);
+		return this;
+	}
+	
+	@Override
+	public ColorAttachmentSpec setLinearFilteringEnabled(boolean enabled)
+	{
+		super.setLinearFilteringEnabled(enabled);
+		return this;
+	}
 }
--- Java/ReflectanceAcquisition/src/tetzlaff/gl/builders/framebuffer/StencilAttachmentSpec.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/gl/builders/framebuffer/StencilAttachmentSpec.java
@@ -8,4 +8,25 @@ public class StencilAttachmentSpec extends AttachmentSpec
 	{
 		this.precision = precision;
 	}
+	
+	@Override
+	public StencilAttachmentSpec setMultisamples(int samples, boolean fixedSampleLocations)
+	{
+		super.setMultisamples(samples, fixedSampleLocations);
+		return this;
+	}
+
+	@Override
+	public StencilAttachmentSpec setMipmapsEnabled(boolean enabled)
+	{
+		super.setMipmapsEnabled(enabled);
+		return this;
+	}
+	
+	@Override
+	public StencilAttachmentSpec setLinearFilteringEnabled(boolean enabled)
+	{
+		super.setLinearFilteringEnabled(enabled);
+		return this;
+	}
 }
