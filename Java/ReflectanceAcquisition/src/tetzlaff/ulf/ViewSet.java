e2c657d6577a3052a720ae5e34d240c4a883788d
Seth Berrier <berriers@uwstout.edu>
2015-09-27 09:39:26 -0500

    Updates for Alpha v2: warning about known issue, background, beuhler mode - Added an interface 'MessageBox' to abstract away standard message dialogs - Added a warning dialog if the user tries to load a model that might not work - Updated renderer to allow setting of background color and to enable k-neighbors mode - In k-neighbors mode, only top k samples are used (k is settable) - Updated gui to expose controls for background color and k-neighbors mode

--- Java/ReflectanceAcquisition/src/tetzlaff/ulf/ViewSet.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/ulf/ViewSet.java
@@ -32,6 +32,8 @@ import tetzlaff.gl.helpers.Matrix3;
 import tetzlaff.gl.helpers.Matrix4;
 import tetzlaff.gl.helpers.Vector3;
 import tetzlaff.helpers.ZipWrapper;
+import tetzlaff.interactive.InteractiveApplication;
+import tetzlaff.interactive.MessageBox;
 
 public class ViewSet<ContextType extends Context<ContextType>>
 {
@@ -586,6 +588,7 @@ public class ViewSet<ContextType extends Context<ContextType>>
         Camera camera = null;
         float globalScale = 1.0f;
         Matrix4 globalRotation = new Matrix4();
+        boolean globalRotationSet = false, globalScaleSet = false;
         
         String version = "", chunkLabel = "", groupLabel = "";
         String sensorID = "", cameraID = "", imageFile = "";
@@ -786,6 +789,7 @@ public class ViewSet<ContextType extends Context<ContextType>>
                                     if(expectedSize == 9)
                                     {
                                         System.out.println("\tSetting global rotation.");
+                                        globalRotationSet = true;
                                     	globalRotation = new Matrix4(new Matrix3(
                                             m[0], m[3], m[6],
                                             m[1], m[4], m[7],
@@ -794,6 +798,7 @@ public class ViewSet<ContextType extends Context<ContextType>>
                                     else
                                     {
                                         System.out.println("\tSetting global transformation.");
+                                        globalRotationSet = true;
                                     	globalRotation = new Matrix4(new Matrix3(
 	                                             m[0], 	m[4],  m[8],
 	                                             m[1],  m[5],  m[9],
@@ -808,6 +813,7 @@ public class ViewSet<ContextType extends Context<ContextType>>
                         	if (camera == null)
                         	{
                                 System.out.println("\tSetting global scale.");
+                                globalScaleSet = true;
                     			globalScale = 1.0f/Float.parseFloat(reader.getElementText());
                         	}
                         	break;
@@ -872,6 +878,23 @@ public class ViewSet<ContextType extends Context<ContextType>>
             e.printStackTrace();
         }
         
+        // Check for known troublesome cases and warn user
+        if(globalRotationSet && globalScaleSet)
+        {
+        	MessageBox userMessage = InteractiveApplication.getMessageBox();
+        	if(userMessage != null)
+        	{
+        		userMessage.warning(
+        			"Known Issue: Global Rotation and Scale",
+        			"This model has both a global rotation and a global scale. " +
+        			"There is a known problem with the ULF Renderer that these types " +
+        			"of modesl sometimes do not work.\n\nIf you experience problems please " +
+        			"let us know and consider submitting the model for further testing.\n" + 
+        			"(Select 'help -> about' for contact details.)");
+    		}
+        }
+
+        // Initialize internal lists
         List<Matrix4> cameraPoseList = new ArrayList<Matrix4>();
         List<Matrix4> cameraPoseInvList = new ArrayList<Matrix4>();
 		List<Projection> cameraProjectionList = new ArrayList<Projection>();
@@ -900,7 +923,7 @@ public class ViewSet<ContextType extends Context<ContextType>>
         		sensors[i].k3
     		));
         }
-        
+                
         Camera[] cameras = cameraSet.toArray(new Camera[0]);
         
         // Fill out the camera pose, projection index, and light index lists
