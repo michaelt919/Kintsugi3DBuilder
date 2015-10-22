cc9c7dbec7c0067b03ca7586c78a49d2b4d5f76c
michaelt919 <4734191+michaelt919@users.noreply.github.com>
2015-10-22 14:44:05 -0500

    Added documentation for tetzlaff.helpers package.

--- Java/ReflectanceAcquisition/src/tetzlaff/helpers/SelectableList.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/helpers/SelectableList.java
@@ -2,6 +2,12 @@ package tetzlaff.helpers;
 
 import java.util.List;
 
+/**
+ * An interface for a collection which combines the functionality of a List and a Selectable.
+ * @author Michael Tetzlaff
+ *
+ * @param <T> The type of objects stored in this collection.
+ */
 public interface SelectableList<T> extends List<T>, Selectable<T>
 {
 }
