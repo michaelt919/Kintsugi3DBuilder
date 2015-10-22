f2f86876257ef40c2a4cf0189a5b53bc77ed8a23
michaelt919 <michaelt919@localhost>
2015-10-22 11:25:00 -0500

    Added some documentation to tetzlaff.ulf package.

--- Java/ReflectanceAcquisition/src/tetzlaff/ulf/UnstructuredLightField.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/ulf/UnstructuredLightField.java
@@ -14,23 +14,70 @@ import tetzlaff.gl.VertexBuffer;
 import tetzlaff.gl.exceptions.GLException;
 import tetzlaff.gl.helpers.VertexMesh;
 
+/**
+ * A class for organizing all of the GL resources that are necessary for light field rendering.
+ * This includes a proxy geometry and depth textures for visibility testing, in addition to a view set.
+ * @author Michael Tetzlaff
+ *
+ * @param <ContextType> The type of the context that the GL resources for this unstructured light field are to be used with.
+ */
 public class UnstructuredLightField<ContextType extends Context<ContextType>>
 {
-	public final File directoryPath;
+	/**
+	 * An arbitrary user-friendly identifier.
+	 */
 	public final String id;
+	
+	/**
+	 * The view set containing the light field images and their associated camera poses.
+	 */
 	public final ViewSet<ContextType> viewSet;
+	
+	/**
+	 * The light field proxy geometry.
+	 */
 	public final VertexMesh proxy;
+	
+	/**
+	 * A vertex buffer containing the positions of the vertices in the proxy geometry as 3D vectors.
+	 */
 	public final VertexBuffer<ContextType> positionBuffer;
+	
+	/**
+	 * A vertex buffer containing the texture coordinates of the vertices in the proxy geometry as 2D vectors.
+	 */
 	public final VertexBuffer<ContextType> texCoordBuffer;
+	
+	/**
+	 * A vertex buffer containing the surface normals of the vertices in the proxy geometry as 3D vectors.
+	 */
 	public final VertexBuffer<ContextType> normalBuffer;
+	
+	/**
+	 * A texture array containing a depth image for each view.
+	 */
 	public final Texture3D<ContextType> depthTextures;
+	
+	/**
+	 * The current rendering settings.
+	 */
     public final ULFSettings settings;
 	
-	public UnstructuredLightField(File directoryPath, ViewSet<ContextType> viewSet, VertexMesh proxy, Texture3D<ContextType> depthTextures, 
+    /**
+     * Creates a new unstructured light field.
+     * @param id An arbitrary user-friendly identifier.
+     * @param viewSet The view set containing the light field images and their associated camera poses.
+     * @param proxy The light field proxy geometry.
+     * @param depthTextures A texture array containing a depth image for each view.
+     * @param positionBuffer A vertex buffer containing the positions of the vertices in the proxy geometry as 3D vectors.
+     * @param texCoordBuffer A vertex buffer containing the texture coordinates of the vertices in the proxy geometry as 2D vectors.
+     * @param normalBuffer A vertex buffer containing the surface normals of the vertices in the proxy geometry as 3D vectors.
+     * @param settings The current rendering settings.
+     */
+	public UnstructuredLightField(String id, ViewSet<ContextType> viewSet, VertexMesh proxy, Texture3D<ContextType> depthTextures, 
 			VertexBuffer<ContextType> positionBuffer, VertexBuffer<ContextType> texCoordBuffer, VertexBuffer<ContextType> normalBuffer, ULFSettings settings) 
 	{
-    	this.id = directoryPath.getName();
-		this.directoryPath = directoryPath;
+    	this.id = id;
 		this.viewSet = viewSet;
 		this.proxy = proxy;
 		this.depthTextures = depthTextures;
@@ -40,6 +87,10 @@ public class UnstructuredLightField<ContextType extends Context<ContextType>>
 		this.settings = settings;
 	}
 	
+	/**
+	 * Deletes all GL resources associated with this light field (including resources associated with the underlying view set).
+	 * Attempting to use these resources after calling this method will have undefined results.
+	 */
 	public void deleteOpenGLResources()
 	{
     	this.positionBuffer.delete();
@@ -61,13 +112,18 @@ public class UnstructuredLightField<ContextType extends Context<ContextType>>
 			depthTextures.delete();
 		}
 	}
-	
-	public static <ContextType extends Context<ContextType>> 
-		UnstructuredLightField<ContextType> loadFromAgisoftXMLFile(File xmlFile, File meshFile, ContextType context) throws IOException
-	{
-		return UnstructuredLightField.loadFromAgisoftXMLFile(xmlFile, meshFile, null);
-	}
 
+	/**
+	 * Loads a camera definition file exported in XML format from Agisoft PhotoScan along with a specific geometric proxy 
+	 * and initializes a corresponding UnstructuredLightField object with all associated GPU resources.
+	 * @param xmlFile The Agisoft PhotoScan XML camera file to load.
+	 * @param meshFile The mesh exported from Agisoft PhotoScan to be used as proxy geometry.
+	 * @param loadOptions The requested options for loading the unstructured light field.
+	 * @param context The GL context in which to create the resources.
+	 * @param loadingCallback A callback for monitoring loading progress, particularly for images.
+	 * @return The newly created UnstructuredLightField object.
+	 * @throws IOException Thrown due to a File I/O error occurring.
+	 */
 	public static <ContextType extends Context<ContextType>>
 		UnstructuredLightField<ContextType> loadFromAgisoftXMLFile(File xmlFile, File meshFile, ULFLoadOptions loadOptions, ContextType context, ULFLoadingMonitor loadingCallback) throws IOException
 	{
@@ -145,15 +201,18 @@ public class UnstructuredLightField<ContextType extends Context<ContextType>>
         	normalBuffer = context.createVertexBuffer().setData(proxy.getNormals());
         }
         
-    	return new UnstructuredLightField<ContextType>(directoryPath, viewSet, proxy, depthTextures, positionBuffer, texCoordBuffer, normalBuffer, new ULFSettings());
+    	return new UnstructuredLightField<ContextType>(directoryPath.getName(), viewSet, proxy, depthTextures, positionBuffer, texCoordBuffer, normalBuffer, new ULFSettings());
 	}
 	
-	public static <ContextType extends Context<ContextType>>
-		UnstructuredLightField<ContextType> loadFromVSETFile(File vsetFile, ContextType context, ULFLoadingMonitor loadingCallback) throws IOException
-	{
-		return UnstructuredLightField.loadFromVSETFile(vsetFile, new ULFLoadOptions(new ViewSetImageOptions(null, false, false, false), false, 0, 0), context, loadingCallback);
-	}
-
+	/**
+	 * Loads a VSET file and creates and initializes a corresponding UnstructuredLightField object with all associated GPU resources.
+	 * @param vsetFile The VSET file to load.
+	 * @param loadOptions The requested options for loading the unstructured light field.
+	 * @param contextThe GL context in which to create the resources.
+	 * @param loadingCallback A callback for monitoring loading progress, particularly for images.
+	 * @return The newly created UnstructuredLightField object.
+	 * @throws IOException Thrown due to a File I/O error occurring.
+	 */
 	public static <ContextType extends Context<ContextType>>
 		UnstructuredLightField<ContextType> loadFromVSETFile(File vsetFile, ULFLoadOptions loadOptions, ContextType context, ULFLoadingMonitor loadingCallback) throws IOException
 	{
@@ -232,7 +291,7 @@ public class UnstructuredLightField<ContextType extends Context<ContextType>>
         	normalBuffer = context.createVertexBuffer().setData(proxy.getNormals());
         }
         
-    	return new UnstructuredLightField<ContextType>(directoryPath, viewSet, proxy, depthTextures, positionBuffer, texCoordBuffer, normalBuffer, new ULFSettings());
+    	return new UnstructuredLightField<ContextType>(directoryPath.getName(), viewSet, proxy, depthTextures, positionBuffer, texCoordBuffer, normalBuffer, new ULFSettings());
 	}
 	
 	@Override
--- Java/ReflectanceAcquisition/src/tetzlaff/ulf/ViewSetImageOptions.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/ulf/ViewSetImageOptions.java
@@ -2,13 +2,40 @@ package tetzlaff.ulf;
 
 import java.io.File;
 
+/**
+ * A model of the available options when loading the images for a view set.
+ * @author Michael Tetzlaff
+ *
+ */
 public class ViewSetImageOptions 
 {
+	/**
+	 * The absolute file path of the images.
+	 */
 	private File filePath;
+	
+	/**
+	 * Whether or not images should be automatically loaded.
+	 */
 	private boolean loadingRequested;
+	
+	/**
+	 * Whether or not mipmaps should be generated for images.
+	 */
 	private boolean mipmapsRequested;
+	
+	/**
+	 * Whether or not images should be hardware-compressed.
+	 */
 	private boolean compressionRequested;
 	
+	/**
+	 * Creates a new object for modeling view set image loading options.
+	 * @param filePath The absolute file path of the images.
+	 * @param loadingRequested Whether or not images should be automatically loaded.
+	 * @param mipmapsRequested Whether or not mipmaps should be generated for images.
+	 * @param compressionRequested Whether or not images should be hardware-compressed.
+	 */
 	public ViewSetImageOptions(File filePath, boolean loadingRequested, boolean mipmapsRequested, boolean compressionRequested) 
 	{
 		this.filePath = filePath;
@@ -17,41 +44,73 @@ public class ViewSetImageOptions
 		this.compressionRequested = compressionRequested;
 	}
 
+	/**
+	 * Gets the absolute file path of the images.
+	 * @return The absolute file path.
+	 */
 	public File getFilePath() 
 	{
 		return this.filePath;
 	}
 
+	/**
+	 * Sets the absolute file path of the images.
+	 * @param filePath The absolute file path.
+	 */
 	public void setFilePath(File filePath) 
 	{
 		this.filePath = filePath;
 	}
 
+	/**
+	 * Gets whether or not images should be automatically loaded.
+	 * @return true if images should be loaded, false otherwise.
+	 */
 	public boolean isLoadingRequested() 
 	{
 		return this.loadingRequested;
 	}
 
+	/**
+	 * Sets whether or not images should be automatically loaded.
+	 * @param loadingRequested true if images should be loaded, false otherwise.
+	 */
 	public void setLoadingRequested(boolean loadingRequested) 
 	{
 		this.loadingRequested = loadingRequested;
 	}
 
+	/**
+	 * Gets whether or not mipmaps should be generated for images.
+	 * @return true if mipmaps should be generated, false otherwise.
+	 */
 	public boolean areMipmapsRequested() 
 	{
 		return this.mipmapsRequested;
 	}
 
+	/**
+	 * Sets whether or not mipmaps should be generated for images.
+	 * @param mipmapsRequested true if mipmaps should be generated, false otherwise.
+	 */
 	public void setMipmapsRequested(boolean mipmapsRequested) 
 	{
 		this.mipmapsRequested = mipmapsRequested;
 	}
 
+	/**
+	 * Gets whether or not images should be hardware-compressed.
+	 * @return true if images should be compressed, false otherwise.
+	 */
 	public boolean isCompressionRequested() 
 	{
 		return this.compressionRequested;
 	}
 
+	/**
+	 * Sets whether or not images should be hardware-compressed.
+	 * @param compressionRequested true if images should be compressed, false otherwise.
+	 */
 	public void setCompressionRequested(boolean compressionRequested) 
 	{
 		this.compressionRequested = compressionRequested;
