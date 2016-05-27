5f285b26dccae4bd813c561214d8023fd3249ab2
michaelt919 <4734191+michaelt919@users.noreply.github.com>
2016-05-27 14:09:58 -0500

    Added license documentation to each source file.

--- Java/ReflectanceAcquisition/src/tetzlaff/gl/helpers/ByteVertexList.java
+++ Java/ReflectanceAcquisition/src/tetzlaff/gl/helpers/ByteVertexList.java
@@ -1,3 +1,28 @@
+/*
+ * LF Viewer - A tool to render Agisoft PhotoScan models as light fields.
+ *
+ * Copyright (c) 2016
+ * The Regents of the University of Minnesota
+ *     and
+ * Cultural Heritage Imaging
+ * All rights reserved
+ *
+ * This file is part of LF Viewer.
+ *
+ *     LF Viewer is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     LF Viewer is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with LF Viewer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package tetzlaff.gl.helpers;
 
 import java.nio.ByteBuffer;
