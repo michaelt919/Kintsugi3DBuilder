2704dc459bb1cd402d69dabf593f855e6bbd2772
michaelt919 <4734191+michaelt919@users.noreply.github.com>
2017-05-25 18:38:28 -0500

    Added missing files from previous commit.

---
+++ src/tetzlaff/gl/glfw/GLFWWindowContextBase.java
@@ -0,0 +1,45 @@
+package tetzlaff.gl.glfw;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+import java.nio.ByteBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL;
+
+import tetzlaff.gl.Context;
+import tetzlaff.gl.FramebufferSize;
+
+public abstract class GLFWWindowContextBase<ContextType extends GLFWWindowContextBase<ContextType>> implements Context<ContextType>
+{
+	private final long handle;
+	
+	protected GLFWWindowContextBase(long handle)
+	{
+		this.handle = handle;
+	}
+	
+	@Override
+	public void makeContextCurrent()
+	{
+		glfwMakeContextCurrent(handle);
+		GL.createCapabilities(false);
+	}
+	
+	@Override
+	public void swapBuffers()
+	{
+		glfwSwapBuffers(handle);
+	}
+	
+	@Override
+	public FramebufferSize getFramebufferSize()
+	{
+		ByteBuffer widthBuffer = BufferUtils.createByteBuffer(4);
+		ByteBuffer heightBuffer = BufferUtils.createByteBuffer(4);
+		glfwGetFramebufferSize(handle, widthBuffer, heightBuffer);
+		int width = widthBuffer.asIntBuffer().get(0);
+		int height = heightBuffer.asIntBuffer().get(0);
+		return new FramebufferSize(width, height);
+	}
+}
---
+++ src/tetzlaff/gl/window/WindowBuilderBase.java
@@ -0,0 +1,84 @@
+package tetzlaff.gl.window;
+
+public abstract class WindowBuilderBase<WindowType extends Window<?>> implements WindowBuilder<WindowType>
+{
+	private String title;
+	private int width;
+	private int height;
+	private int x;
+	private int y;
+	private boolean resizable = false;
+	private int multisamples = 0;
+	
+	protected WindowBuilderBase(String title, int width, int height, int x, int y)
+	{
+		this.title = title;
+		this.width = width;
+		this.height = height;
+		this.x = x;
+		this.y = y;
+	}
+	
+	protected String getTitle() 
+	{
+		return title;
+	}
+	
+	protected int getWidth() 
+	{
+		return width;
+	}
+	
+	protected int getHeight() 
+	{
+		return height;
+	}
+	
+	protected int getX() 
+	{
+		return x;
+	}
+
+	protected int getY() 
+	{
+		return y;
+	}
+
+	protected boolean isResizable() 
+	{
+		return resizable;
+	}
+
+	protected int getMultisamples()
+	{
+		return multisamples;
+	}
+
+	@Override
+	public WindowBuilderBase<WindowType> setX(int x) 
+	{
+		this.x = x;
+		return this;
+	}
+
+	@Override
+	public WindowBuilderBase<WindowType> setY(int y)
+	{
+		this.y = y;
+		return this;
+	}
+
+	@Override
+	public WindowBuilderBase<WindowType> setResizable(boolean resizable) 
+	{
+		this.resizable = resizable;
+		return this;
+	}
+
+	@Override
+	public WindowBuilderBase<WindowType> setMultisamples(int multisamples) 
+	{
+		this.multisamples = multisamples;
+		return this;
+	}
+}
