4dc6035b8322225966567afcdfc66aada3693a00
Alex Kautz <16840146+AlexKautz@users.noreply.github.com>
2017-06-22 14:45:28 -0500

    renamed my packige -Alex Kautz

---
+++ src/tetzlaff/ibr/alexkautz_workspace/mount_olympus/RunMe.java
@@ -0,0 +1,28 @@
+package tetzlaff.ibr.alexkautz_workspace.mount_olympus;
+
+public class RunMe {
+    public static void main(String[] args) {
+        System.out.println("Alex Kautz");
+        System.out.println("Start Main");
+
+        System.out.println("Initlising Paramiters");
+        Parameters.init(null, "Hello World");
+
+        System.out.println("Starting JavaFX UI");
+        startJavaFXUI();
+
+        System.out.println("Starting Render Window");
+        startRenderWindow();
+
+        System.out.println("End Main");
+    }
+
+    private static void startJavaFXUI(){
+
+    }
+
+    private static void startRenderWindow(){
+
+    }
+
+}
