242ed2d5d7fee3b275c845149aec33984de8de7d
alexk <alexk@DESKTOP-NR5D0B1.umn.edu>
2017-07-18 11:34:53 -0500

    More work on light settings backend (03)      -AK

--- src/tetzlaff/ibr/gui2/controllers/scene/lights/LightGroupSetting.java
+++ src/tetzlaff/ibr/gui2/controllers/scene/lights/LightGroupSetting.java
@@ -1,67 +1,86 @@
 package tetzlaff.ibr.gui2.controllers.scene.lights;//Created by alexk on 7/16/2017.
 
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+
+import com.sun.javafx.collections.ObservableListWrapper;
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class LightGroupSetting {
-    private final static int L =4; //max number of lights
+    public final static int LIGHT_LIMIT = 4;
+
+    private final ListProperty<LightSetting> lightList = new SimpleListProperty<>(
+            new ObservableListWrapper<LightSetting>(
+                    new ArrayList<>(LIGHT_LIMIT)
+            )
+    );
+
+    private final BooleanProperty locked = new SimpleBooleanProperty(false);
+
     private final StringProperty name = new SimpleStringProperty();
-    private final BooleanProperty groupLocked = new SimpleBooleanProperty();
-    private final ArrayList<LightSetting> lights = new ArrayList<>(L);
 
-
-
-    public LightSetting getLight(int index){
-        assert index < lights.size() : "try'd to get a light that doesn't exist yet!\n index:" + index
-                + " nLights:" + lights.size() + " groopName:" + name.getValue();
-        return lights.get(index);
+    public LightGroupSetting(String name) {
+        this.name.setValue(name);
     }
 
-    public void addLight(int duplicateFromIndex){
-        //If this is the first light added, we will create it with default settings
-        //Otherwise, we will duplicate the previous light.
-        //we will also prevent creating too many lights here.
-        LightSetting newLight;
-        if(lights.size() == 0){
-            newLight = new LightSetting(
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    90.0,
-                    1.0,
-                    0.0,
-                    false,
-                    ("L1"),
-                    LightType.PointLight,
-                    groupLocked
+    public void addLight(){
+        if(lightList.size() < LIGHT_LIMIT){
+            lightList.add(
+                    new LightSetting(
+                            0.0,
+                            0.0,
+                            0.0,
+                            0.0,
+                            90.0,
+                            1.0,
+                            1.0,
+                            false,
+                            ("L" + lightList.size()),
+                            LightType.PointLight,
+                            locked
+                    )
             );
         }
-        else if(lights.size() < L && duplicateFromIndex < lights.size()){
-            newLight = lights.get(duplicateFromIndex).duplicate();
-        }
-        else if(lights.size() < L){
-            newLight = lights.get(lights.size()-1).duplicate();
-        }
-        else return;//do nothing (since nLights=L)
-
-        assert newLight != null;
-
-        lights.add(newLight);
     }
 
-    public void removeLight(int removeFromIndex){
-        if(lights.size() == 0) return;
-        else if (removeFromIndex < lights.size()){
-           lights.remove(removeFromIndex);
-        } else {
-            lights.remove(lights.size()-1);
+    public void addLight(int index){
+        if(lightList.size() < LIGHT_LIMIT){
+            if(index < lightList.size()) lightList.add(lightList.get(index).duplicate());
+            else addLight();
         }
+    }
+
+    public void removeLight(){
+        if(lightList.size() > 0) lightList.remove(lightList.size() -1);
+    }
+
+    public void removeLight(int index){
+        if(lightList.size() > 0){
+            if(index<lightList.size()) lightList.remove(index);
+            else removeLight();
+        }
+    }
+
+
+    public ObservableList<LightSetting> getLightList() {
+        return lightList.get();
+    }
+
+    public ListProperty<LightSetting> lightListProperty() {
+        return lightList;
+    }
+
+    public boolean isLocked() {
+        return locked.get();
+    }
+
+    public BooleanProperty lockedProperty() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked.set(locked);
     }
 
     public String getName() {
@@ -74,17 +93,5 @@ public class LightGroupSetting {
 
     public void setName(String name) {
         this.name.set(name);
-    }
-
-    public boolean isGroupLocked() {
-        return groupLocked.get();
-    }
-
-    public BooleanProperty groupLockedProperty() {
-        return groupLocked;
-    }
-
-    public void setGroupLocked(boolean groupLocked) {
-        this.groupLocked.set(groupLocked);
     }
 }
--- src/tetzlaff/ibr/gui2/controllers/scene/lights/RootLightSceneController.java
+++ src/tetzlaff/ibr/gui2/controllers/scene/lights/RootLightSceneController.java
@@ -1,12 +1,46 @@
 package tetzlaff.ibr.gui2.controllers.scene.lights;//Created by alexk on 7/16/2017.
 
+import com.sun.javafx.collections.ObservableListWrapper;
+import com.sun.javafx.scene.control.skin.TableHeaderRow;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.binding.ListExpression;
+import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import com.sun.javafx.collections.ObservableListWrapper;
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
+
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class RootLightSceneController implements Initializable {
@@ -17,8 +51,117 @@ public class RootLightSceneController implements Initializable {
     @FXML private VBox lightControls;
     @FXML private Button theRenameButton;
 
+    private final ObservableList<LightGroupSetting> lightGroups = new ObservableListWrapper<>(new ArrayList<>());
+    private final Property<LightSetting> selectedLight = new SimpleObjectProperty<>();
+    private int lastSelectedIndex = -1;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("GLSC started!");
+
+        //TABLE SET UP
+        //columns
+        TableColumn<LightGroupSetting, String> nameCol = new TableColumn<>("Name");
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableView.getColumns().add(nameCol);
+
+        for (int i = 0; i < LightGroupSetting.LIGHT_LIMIT; i++) {
+            final Integer tempFinalInt = i;
+
+            TableColumn<LightGroupSetting, LightSetting> newCol = new TableColumn<>("L" + (tempFinalInt+1));
+
+            newCol.setCellValueFactory(param -> param.getValue().lightListProperty().valueAt(tempFinalInt));
+
+            tableView.getColumns().add(newCol);
+        }
+
+
+
+        tableView.getSelectionModel().setCellSelectionEnabled(true);
+
+        //light selection listener
+        tableView.getSelectionModel().getSelectedCells().addListener((ListChangeListener<TablePosition>) c -> {
+            while (c.next()){
+                if(c.wasAdded()){
+                    //new cell selected
+                    assert c.getAddedSize() == 1;
+                    TablePosition tb = c.getAddedSubList().get(0);
+                    ObservableValue selected = tb.getTableColumn().getCellObservableValue(tb.getRow());
+                    if(selected != null){
+                        if(selected.getValue() instanceof LightSetting){
+                            selectedLight.setValue((LightSetting) selected.getValue());
+                            lastSelectedIndex = tb.getColumn()-1;
+                        }
+                        else if(selected.getValue() instanceof String){
+                            selectedLight.setValue(null);
+                            lastSelectedIndex = -1;
+                        }
+                    }
+                }
+            }
+        });
+
+        //preventing reordering or rearranging
+        tableView.skinProperty().addListener((obs, oldS, newS)->{
+            final TableHeaderRow tableHeaderRow = (TableHeaderRow) tableView.lookup("TableHeaderRow");
+            tableHeaderRow.reorderingProperty().addListener((p, o, n)->
+            tableHeaderRow.setReordering(false));
+        });
+
+        tableView.setSortPolicy(param -> false);
+
+        tableView.setColumnResizePolicy(param -> false);
+
+        tableView.setItems(lightGroups);
+
+        //TABLE SET UP DONE
+
+        lightGroups.add(new LightGroupSetting("Free Lights"));
+
+
+        selectedLight.addListener(settingsController.changeListener);
+
+
+
     }
+
+    @FXML private void newGroup(){
+        System.out.println("TODO newGroupButton");//TODO
+    }
+    @FXML private void saveGroup(){
+        System.out.println("TODO saveGroup");//TODO
+    }
+    @FXML private void renameGroup(){
+        System.out.println("TODO renameGroup");//TODO
+    }
+    @FXML private void moveUPGroup(){
+        System.out.println("TODO moveUPGroup");//TODO
+    }
+    @FXML private void moveDOWNGroup(){
+        System.out.println("TODO moveDOWNGroup");//TODO
+    }
+    @FXML private void lockGroup(){
+        System.out.println("TODO lockGroup");//TODO
+    }
+    @FXML private void keyframeGroup(){
+        System.out.println("TODO keyframeGroup");//TODO
+    }
+    @FXML private void deleteGroup(){
+        System.out.println("TODO deleteGroup");//TODO
+    }
+    @FXML private void newLight(){
+        System.out.println("TODO newLight");//TODO
+    }
+    @FXML private void saveLight(){
+        System.out.println("TODO saveLight");//TODO
+    }
+    @FXML private void lockLight(){
+        System.out.println("TODO lockLight");//TODO
+    }
+    @FXML private void deleteLight(){
+        System.out.println("TODO deleteLight");//TODO
+    }
+
+
+
+
 }
