3390d77d1f5440166f425d0d1769c7186d4c546d
michaelt919 <4734191+michaelt919@users.noreply.github.com>
2017-08-01 16:51:45 -0500

    Eliminated intensity-based weighting from specular roughness fit.  Re-enabled BTF export for use for the upcoming DARPA review.

--- src/tetzlaff/ibr/util/BTFRequest.java
+++ src/tetzlaff/ibr/util/BTFRequest.java
@@ -29,6 +29,7 @@ public class BTFRequest implements IBRRequest
 		this.height = height;
 		this.exportPath = exportPath;
 		this.settings = settings;
+		this.lightColor = lightColor;
 	}
 	
 	@Override
@@ -76,26 +77,26 @@ public class BTFRequest implements IBRRequest
 	    	
 	    	////////////////////////////////
 	    	
-	    	// Backscattering
-			for (int i = 1; i <= 179; i++)
+//	    	// Backscattering
+//			for (int i = 1; i <= 179; i++)
+//			{
+//				double theta = i / 180.0f * Math.PI;
+//		    	btfProgram.setUniform("virtualLightCount", 1);
+//		    	btfProgram.setUniform("lightIntensityVirtual[0]", lightColor);
+//		    	btfProgram.setUniform("lightDirTSOverride", new Vector3((float)Math.cos(theta), 0.0f, (float)Math.sin(theta)));
+//		    	btfProgram.setUniform("viewDirTSOverride", new Vector3((float)Math.cos(theta), 0.0f, (float)Math.sin(theta)));
+	    	
+	    	// Joey's lab
+	    	for (int i = 1; i <= 90; i++)
 			{
 				double theta = i / 180.0f * Math.PI;
 		    	btfProgram.setUniform("virtualLightCount", 1);
 		    	btfProgram.setUniform("lightIntensityVirtual[0]", lightColor);
-		    	btfProgram.setUniform("lightDirTSOverride", new Vector3((float)Math.cos(theta), 0.0f, (float)Math.sin(theta)));
-		    	btfProgram.setUniform("viewDirTSOverride", new Vector3((float)Math.cos(theta), 0.0f, (float)Math.sin(theta)));
-	    	
-//	    	// Joey's lab
-//	    	for (int i = 1; i <= 90; i++)
-//			{
-//				double theta = i / 180.0f * Math.PI;
-//		    	btfProgram.setUniform("virtualLightCount", 1);
-//		    	btfProgram.setUniform("lightIntensityVirtual[0]", lightController.getLightColor(0));
-////		    	btfProgram.setUniform("lightDirTSOverride", new Vector3(-(float)(Math.sin(theta)*Math.sqrt(0.5)), -(float)(Math.sin(theta)*Math.sqrt(0.5)), (float)Math.cos(theta)));
-////		    	btfProgram.setUniform("viewDirTSOverride", new Vector3((float)(Math.cos(theta)*Math.sqrt(0.5)), (float)(Math.cos(theta)*Math.sqrt(0.5)), (float)Math.sin(theta)));
+		    	btfProgram.setUniform("lightDirTSOverride", new Vector3(-(float)(Math.sin(theta)*Math.sqrt(0.5)), -(float)(Math.sin(theta)*Math.sqrt(0.5)), (float)Math.cos(theta)));
+		    	btfProgram.setUniform("viewDirTSOverride", new Vector3((float)(Math.cos(theta)*Math.sqrt(0.5)), (float)(Math.cos(theta)*Math.sqrt(0.5)), (float)Math.sin(theta)));
 //		    	btfProgram.setUniform("lightDirTSOverride", new Vector3(-(float)Math.sin(theta), 0.0f, (float)Math.cos(theta)));
 //		    	btfProgram.setUniform("viewDirTSOverride", new Vector3((float)Math.cos(theta), 0.0f, (float)Math.sin(theta)));
-//		    	
+		    	
 	    	////////////////////////////////
 				
 		    	context.getState().disableBackFaceCulling();
