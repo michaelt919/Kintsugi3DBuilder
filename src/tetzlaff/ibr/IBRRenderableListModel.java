00bca2468807894b7ec482da9fd57c28dc7dca8f
alexk <alexk@DESKTOP-NR5D0B1.umn.edu>
2017-08-07 15:45:32 -0500

    added basic v-set loading
       -AK

--- src/tetzlaff/ibr/IBRRenderableListModel.java
+++ src/tetzlaff/ibr/IBRRenderableListModel.java
@@ -10,7 +10,7 @@ import tetzlaff.ibr.rendering2.to_sort.IBRLoadOptions2;
 
 public interface IBRRenderableListModel<ContextType extends Context<ContextType>> extends ComboBoxModel<IBRRenderable<ContextType>>
 {
-	IBRRenderable<ContextType> addFromVSETFile(String id, File vsetFile, IBRLoadOptions loadOptions) throws IOException;
+	IBRRenderable<ContextType> addFromVSETFile(String id, File vsetFile, IBRLoadOptions2 loadOptions) throws IOException;
 	IBRRenderable<ContextType> addFromAgisoftXMLFile(String id, File xmlFile, File meshFile, File undistortedImageDirectory, IBRLoadOptions2 loadOptions) throws IOException;
 	@Override
 	IBRRenderable<ContextType> getSelectedItem();
