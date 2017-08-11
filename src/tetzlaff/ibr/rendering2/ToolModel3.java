a01ad7adf66f7cf7c3c26af0831ca5036eabb251
Alex Kautz <16840146+AlexKautz@users.noreply.github.com>
2017-08-11 15:31:23 -0500

    added Quit class to handle quiting the app
    -AK

--- src/tetzlaff/ibr/rendering2/ToolModel3.java
+++ src/tetzlaff/ibr/rendering2/ToolModel3.java
@@ -2,6 +2,8 @@ package tetzlaff.ibr.rendering2;//Created by alexk on 7/24/2017.
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
 import tetzlaff.ibr.gui2.controllers.menu_bar.IBRSettingsUIImpl;
 import tetzlaff.ibr.gui2.controllers.menu_bar.LoadSettings;
 import tetzlaff.ibr.rendering2.to_sort.IBRLoadOptions2;
@@ -9,7 +11,18 @@ import tetzlaff.ibr.rendering2.to_sort.IBRSettingsModel;
 import tetzlaff.ibr.rendering2.tools2.ToolBox;
 import tetzlaff.mvc.models.ControllableToolModel;
 
+
 public class ToolModel3 extends ControllableToolModel {
+    private EventHandler<WindowEvent> closeEvent;
+    public void setCloseEvent(EventHandler<WindowEvent> closeEvent) {
+        this.closeEvent = closeEvent;
+    }
+    @Override
+    protected void requestGUIClose() {
+        if (closeEvent != null) {
+            closeEvent.handle(null);
+        }
+    }
 
     private ObjectProperty<ToolBox.TOOL> tool = new SimpleObjectProperty<>(ToolBox.TOOL.ORBIT);
 
