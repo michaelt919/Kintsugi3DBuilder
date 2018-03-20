da5aa22cc48ec797274602087eeeaeb2661a2b81
Michael <4734191+michaelt919@users.noreply.github.com>
2018-03-20 16:06:36 -0500

    Added UI support for spot lights.  Refactored light instance setting class.

--- src/tetzlaff/ibrelight/javafx/controllers/scene/lights/SettingsLightSceneController.java
+++ src/tetzlaff/ibrelight/javafx/controllers/scene/lights/SettingsLightSceneController.java
@@ -8,7 +8,6 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
@@ -30,7 +29,7 @@ public class SettingsLightSceneController implements Initializable
             @Override
             public String toString(Double object)
             {
-                String s = n10.toString(object);
+                String s = logScaleNumberStringConverter.toString(object);
                 if (s.length() > 4)
                 {
                     return s.substring(0, 4);
@@ -62,6 +61,9 @@ public class SettingsLightSceneController implements Initializable
 
         StaticUtilities.makeWrapAroundNumeric(-180, 180, azimuthTextField);
         StaticUtilities.makeClampedNumeric(-90, 90, inclinationTextField);
+
+        StaticUtilities.makeClampedNumeric(0.0, 90.0, spotSizeTextField);
+        StaticUtilities.makeClampedNumeric(0.0, 1.0, spotTaperTextField);
     }
 
     @FXML private VBox root;
@@ -80,12 +82,15 @@ public class SettingsLightSceneController implements Initializable
     @FXML private TextField intensityTextField;
     @FXML private Slider intensitySlider;
     @FXML private ColorPicker colorPicker;
-    @FXML private ChoiceBox<LightType> lightTypeChoiceBox;
+    @FXML private TextField spotSizeTextField;
+    @FXML private Slider spotSizeSlider;
+    @FXML private TextField spotTaperTextField;
+    @FXML private Slider spotTaperSlider;
 
     private final DoubleProperty trueIntensity = new SimpleDoubleProperty(1);
 
-    private final SafeNumberStringConverter n = new SafeNumberStringConverter(0);
-    private final SafeLogScaleNumberStringConverter n10 = new SafeLogScaleNumberStringConverter(1);
+    private final SafeNumberStringConverter numberStringConverter = new SafeNumberStringConverter(0);
+    private final SafeLogScaleNumberStringConverter logScaleNumberStringConverter = new SafeLogScaleNumberStringConverter(1);
 
     public final ChangeListener<LightInstanceSetting> changeListener = (observable, oldValue, newValue) ->
     {
@@ -97,7 +102,7 @@ public class SettingsLightSceneController implements Initializable
         if (newValue != null)
         {
             bind(newValue);
-            setDisabled(newValue.isLocked() || newValue.isGroupLocked());
+            setDisabled(newValue.locked().get() || newValue.isGroupLocked());
         }
         else
         {
@@ -110,50 +115,49 @@ public class SettingsLightSceneController implements Initializable
         root.setDisable(disabled);
     }
 
-    private void bind(LightInstanceSetting c)
+    private void bind(LightInstanceSetting setting)
     {
-
-        xCenterTextField.textProperty().bindBidirectional(c.targetXProperty(), n);
-        yCenterTextField.textProperty().bindBidirectional(c.targetYProperty(), n);
-        zCenterTextField.textProperty().bindBidirectional(c.targetZProperty(), n);
-        azimuthTextField.textProperty().bindBidirectional(c.azimuthProperty(), n);
-        inclinationTextField.textProperty().bindBidirectional(c.inclinationProperty(), n);
-        distanceTextField.textProperty().bindBidirectional(c.log10DistanceProperty(), n10);
-        intensityTextField.textProperty().bindBidirectional(c.intensityProperty(), n);
-        xCenterSlider.valueProperty().bindBidirectional(c.targetXProperty());
-        yCenterSlider.valueProperty().bindBidirectional(c.targetYProperty());
-        zCenterSlider.valueProperty().bindBidirectional(c.targetZProperty());
-        azimuthSlider.valueProperty().bindBidirectional(c.azimuthProperty());
-        inclinationSlider.valueProperty().bindBidirectional(c.inclinationProperty());
-        distanceSlider.valueProperty().bindBidirectional(c.log10DistanceProperty());
-
-        trueIntensity.bindBidirectional(c.intensityProperty());
-
-        lightTypeChoiceBox.valueProperty().bindBidirectional(c.lightTypeProperty());
-
-        colorPicker.valueProperty().bindBidirectional(c.colorProperty());
+        xCenterTextField.textProperty().bindBidirectional(setting.targetX(), numberStringConverter);
+        yCenterTextField.textProperty().bindBidirectional(setting.targetY(), numberStringConverter);
+        zCenterTextField.textProperty().bindBidirectional(setting.targetZ(), numberStringConverter);
+        azimuthTextField.textProperty().bindBidirectional(setting.azimuth(), numberStringConverter);
+        inclinationTextField.textProperty().bindBidirectional(setting.inclination(), numberStringConverter);
+        distanceTextField.textProperty().bindBidirectional(setting.log10Distance(), logScaleNumberStringConverter);
+        intensityTextField.textProperty().bindBidirectional(setting.intensity(), numberStringConverter);
+        spotSizeTextField.textProperty().bindBidirectional(setting.spotSize(), numberStringConverter);
+        spotTaperTextField.textProperty().bindBidirectional(setting.spotTaper(), numberStringConverter);
+        xCenterSlider.valueProperty().bindBidirectional(setting.targetX());
+        yCenterSlider.valueProperty().bindBidirectional(setting.targetY());
+        zCenterSlider.valueProperty().bindBidirectional(setting.targetZ());
+        azimuthSlider.valueProperty().bindBidirectional(setting.azimuth());
+        inclinationSlider.valueProperty().bindBidirectional(setting.inclination());
+        distanceSlider.valueProperty().bindBidirectional(setting.log10Distance());
+        spotSizeSlider.valueProperty().bindBidirectional(setting.spotSize());
+        spotTaperSlider.valueProperty().bindBidirectional(setting.spotTaper());
+        trueIntensity.bindBidirectional(setting.intensity());
+        colorPicker.valueProperty().bindBidirectional(setting.color());
     }
 
-    private void unbind(LightInstanceSetting c)
+    private void unbind(LightInstanceSetting setting)
     {
-
-        xCenterTextField.textProperty().unbindBidirectional(c.targetXProperty());
-        yCenterTextField.textProperty().unbindBidirectional(c.targetYProperty());
-        zCenterTextField.textProperty().unbindBidirectional(c.targetZProperty());
-        azimuthTextField.textProperty().unbindBidirectional(c.azimuthProperty());
-        inclinationTextField.textProperty().unbindBidirectional(c.inclinationProperty());
-        distanceTextField.textProperty().unbindBidirectional(c.log10DistanceProperty());
-        intensityTextField.textProperty().unbindBidirectional(c.intensityProperty());
-        xCenterSlider.valueProperty().unbindBidirectional(c.targetXProperty());
-        yCenterSlider.valueProperty().unbindBidirectional(c.targetYProperty());
-        zCenterSlider.valueProperty().unbindBidirectional(c.targetZProperty());
-        azimuthSlider.valueProperty().unbindBidirectional(c.azimuthProperty());
-        inclinationSlider.valueProperty().unbindBidirectional(c.inclinationProperty());
-        distanceSlider.valueProperty().unbindBidirectional(c.log10DistanceProperty());
-
-        trueIntensity.unbindBidirectional(c.intensityProperty());
-
-        lightTypeChoiceBox.valueProperty().unbindBidirectional(c.lightTypeProperty());
-        colorPicker.valueProperty().unbindBidirectional(c.colorProperty());
+        xCenterTextField.textProperty().unbindBidirectional(setting.targetX());
+        yCenterTextField.textProperty().unbindBidirectional(setting.targetY());
+        zCenterTextField.textProperty().unbindBidirectional(setting.targetZ());
+        azimuthTextField.textProperty().unbindBidirectional(setting.azimuth());
+        inclinationTextField.textProperty().unbindBidirectional(setting.inclination());
+        distanceTextField.textProperty().unbindBidirectional(setting.log10Distance());
+        intensityTextField.textProperty().unbindBidirectional(setting.intensity());
+        spotSizeTextField.textProperty().unbindBidirectional(setting.spotSize());
+        spotTaperTextField.textProperty().unbindBidirectional(setting.spotTaper());
+        xCenterSlider.valueProperty().unbindBidirectional(setting.targetX());
+        yCenterSlider.valueProperty().unbindBidirectional(setting.targetY());
+        zCenterSlider.valueProperty().unbindBidirectional(setting.targetZ());
+        azimuthSlider.valueProperty().unbindBidirectional(setting.azimuth());
+        inclinationSlider.valueProperty().unbindBidirectional(setting.inclination());
+        distanceSlider.valueProperty().unbindBidirectional(setting.log10Distance());
+        spotSizeSlider.valueProperty().unbindBidirectional(setting.spotSize());
+        spotTaperSlider.valueProperty().unbindBidirectional(setting.spotTaper());
+        trueIntensity.unbindBidirectional(setting.intensity());
+        colorPicker.valueProperty().unbindBidirectional(setting.color());
     }
 }
