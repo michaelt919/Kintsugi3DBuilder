4f0e6bcc669d475bc0728982a7d99a176d7f6f97
Michael <Michael@Michael-PC.umn.edu>
2018-04-02 15:42:07 -0500

    Work in progress on attempt to make environment rendering more efficient when an SVD representation is available.

--- shaders/relight/reflectanceequations.glsl
+++ shaders/relight/reflectanceequations.glsl
@@ -3,6 +3,10 @@
 
 #line 5 3001
 
+#ifndef SMITH_MASKING_SHADOWING
+#define SMITH_MASKING_SHADOWING 0
+#endif
+
 vec3 computeFresnelReflectivityActual(vec3 specularColor, vec3 grazingColor, float hDotV)
 {
     float maxLuminance = dot(grazingColor, vec3(0.2126, 0.7152, 0.0722));
@@ -34,32 +38,37 @@ float computeGeometricAttenuationVCavity(float nDotH, float nDotV, float nDotL, 
     return min(1.0, 2.0 * nDotH * min(nDotV, nDotL) / hDotV);
 }
 
-//vec3 computeGeometricAttenuationSmithBeckmann(vec3 roughness, float nDotV, float nDotL)
-//{
-//    vec3 aV = 1.0 / (roughness * sqrt(1.0 - nDotV * nDotV) / nDotV);
-//    vec3 aVSq = aV * aV;
-//    vec3 aL = 1.0 / (roughness * sqrt(1.0 - nDotL * nDotL) / nDotL);
-//    vec3 aLSq = aL * aL;
-//
-//    return (aV < 1.6 ? (3.535 * aV + 2.181 * aVSq) / (1 + 2.276 * aV + 2.577 * aVSq) : 1.0)
-//            * (aL < 1.6 ? (3.535 * aL + 2.181 * aLSq) / (1 + 2.276 * aL + 2.577 * aLSq) : 1.0);
-//        // ^ See Walter et al. "Microfacet Models for Refraction through Rough Surfaces"
-//        // for this formula
-//}
-
-vec3 computeGeometricAttenuationSmithGGX(vec3 roughness, float nDotV, float nDotL)
+vec3 computeGeometricAttenuationSmithBeckmann(vec3 roughness, float cosine)
 {
-    vec3 roughnessSq = roughness * roughness;
-    return 4 / (1 + sqrt(1 + roughnessSq * (1 / (nDotV * nDotV) - 1.0)))
-             / (1 + sqrt(1 + roughnessSq * (1 / (nDotL * nDotL) - 1.0)));
+    vec3 a = min(1.6, cosine / (roughness * sqrt(1.0 - cosine * cosine)));
+    vec3 aSq = a * a;
+
+    return min(1.0, (3.535 * a + 2.181 * aSq) / (1 + 2.276 * a + 2.577 * aSq));
+    // ^ See Walter et al. "Microfacet Models for Refraction through Rough Surfaces"
+    // for this formula
+}
+
+vec3 computeGeometricAttenuationSmithGGX(vec3 roughness, float cosine)
+{
+    return 2 / (1 + sqrt(1 + roughness * roughness * (1 / (cosine * cosine) - 1.0)));
 }
 
 vec3 geom(vec3 roughness, float nDotH, float nDotV, float nDotL, float hDotV)
 {
-    //return nDotV * nDotL;
-    return vec3(computeGeometricAttenuationVCavity(nDotH, nDotV, nDotL, hDotV));
+    vec3 result;
+#if SMITH_MASKING_SHADOWING
+    result = geomPartial(roughness, nDotL) * geomPartial(roughness, nDotV);
+#else
+    result = vec3(computeGeometricAttenuationVCavity(nDotH, nDotV, nDotL, hDotV));
+#endif
+    return result;
+}
+
+vec3 geomPartial(vec3 roughness, float cosine)
+{
+    //return cosine;
     //return computeGeometricAttenuationSmithBeckmann(roughness, nDotV, nDotL);
-    //return computeGeometricAttenuationSmithGGX(roughness, nDotV, nDotL);
+    return computeGeometricAttenuationSmithGGX(roughness, nDotV, nDotL);
 }
 
 vec3 computeMicrofacetDistributionGGX(float nDotH, vec3 roughness)
