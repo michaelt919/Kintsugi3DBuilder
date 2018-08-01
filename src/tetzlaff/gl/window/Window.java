0ec0864f35e46d81ef8e585daa330939dac46292
michaelt919 <michaelt919@localhost>
2018-08-01 16:38:23 -0500

    Working on JavaFX-based window implementation that uses offscreen LWJGL buffer. (grafted from ba45bd12124275d5b0fd13fa646bab07e5c0338d)

--- src/tetzlaff/gl/window/Window.java
+++ src/tetzlaff/gl/window/Window.java
@@ -4,7 +4,7 @@ import tetzlaff.gl.core.Context;
 
 public interface Window<ContextType extends Context<ContextType>> extends WindowListenerManager, AutoCloseable
 {
-    ContextType getContext();
+    ContextType getBaseContext();
 
     void show();
 
