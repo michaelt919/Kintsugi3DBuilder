b882ea729e42ced4edf31863342c789ba02f5bf8
JoshLyu <47506371+JoshLyu@users.noreply.github.com>
2021-06-26 18:56:20 -0500

    finish PTMrequestUI and its fxml file, added info in export-classses.txt
    TODO:
     java: package com.sun.istack.internal does not exist

--- src/tetzlaff/ibrelight/export/PTMfit/PTMrequest.java
+++ src/tetzlaff/ibrelight/export/PTMfit/PTMrequest.java
@@ -1,4 +1,30 @@
 package tetzlaff.ibrelight.export.PTMfit;
 
-public class PTMrequest {
+import tetzlaff.gl.core.Context;
+import tetzlaff.ibrelight.core.IBRRenderable;
+import tetzlaff.ibrelight.core.IBRRequest;
+import tetzlaff.ibrelight.core.LoadingMonitor;
+import tetzlaff.ibrelight.core.TextureFitSettings;
+import tetzlaff.ibrelight.export.PTMfit.PTMOptimiztion;
+
+import java.io.IOException;
+
+public class PTMrequest implements IBRRequest {
+
+    private TextureFitSettings setting;
+    public PTMrequest(TextureFitSettings settings){
+        setting = settings;
+    }
+
+    @Override
+    public <ContextType extends Context<ContextType>> void executeRequest(IBRRenderable<ContextType> renderable, LoadingMonitor callback) throws Exception {
+        try
+        {
+            new PTMOptimiztion(setting.width, setting.height).createFit(renderable.getResources());
+        }
+        catch(IOException e) // thrown by createReflectanceProgram
+        {
+            e.printStackTrace();
+        }
+    }
 }
