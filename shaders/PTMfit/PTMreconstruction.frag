0dad1c382c178a2bb2d492cf9a5b3b8b76288157
JoshLyu <47506371+JoshLyu@users.noreply.github.com>
2021-07-29 12:26:04 -0500

    fix the reconstruciton shader problem

--- shaders/PTMfit/PTMreconstruction.frag
+++ shaders/PTMfit/PTMreconstruction.frag
@@ -1,57 +1,38 @@
 #version 330
 #include "PTMfit.glsl"
 #include "evaluateBRDF.glsl"
-#line 18 0
+#line 14 0
 
-uniform sampler2D diffuseEstimate;
 uniform sampler2DArray weightMaps;
 
 #ifndef BASIS_COUNT
-#define BASIS_COUNT 8
+#define BASIS_COUNT 6
 #endif
 
-#ifndef MICROFACET_DISTRIBUTION_RESOLUTION
-#define MICROFACET_DISTRIBUTION_RESOLUTION 90
-#endif
-
-layout(location = 0) out vec4 Color;
+layout(location = 0) out float result;
 
 void main()
 {
-    vec3 f0 = vec3(0);
+    float result=0;
+    vec3 lightDisplacement = getLightVector();
+    lightDir=normalize(lightDisplacement);
+    float u=lightDir.x;
+    float v=lightDir.y;
+    float w=lightDir.w;
 
     float weights[BASIS_COUNT];
-
+    float row[BASIS_COUNT];
+    row[0]=1.0f;
+    row[1]=u;
+    row[2]=v;
+    row[3]=w;
+    row[4]=u*u;
+    row[5]=u*v;
     for (int b = 0; b < BASIS_COUNT; b++)
     {
         weights[b] = texture(weightMaps, vec3(fTexCoord, b))[0];
-        f0 += weights[b] * texelFetch(basisFunctions, ivec2(0, b), 0).rgb;
+        result=result+weights[b]*row[b];
     }
 
-    vec3 sqrtF0 = sqrt(f0);
 
-    vec3 sumNumerator = vec3(0);
-    vec3 sumDenominator = vec3(0);
-
-    for (int m = 1; m < MICROFACET_DISTRIBUTION_RESOLUTION; m++)
-    {
-        vec3 f = vec3(0);
-        for (int b = 0; b < BASIS_COUNT; b++)
-        {
-            f += weights[b] * texelFetch(basisFunctions, ivec2(m, b), 0).rgb;
-        }
-
-        float sqrtAngle = float(m) / float(MICROFACET_DISTRIBUTION_RESOLUTION);
-        float nDotH = cos(sqrtAngle * sqrtAngle * PI / 3.0);
-        float nDotHSq = nDotH * nDotH;
-
-        vec3 numerator = (1.0 - nDotHSq) * sqrt(f);
-        vec3 denominator = sqrt(f0) - nDotHSq * sqrt(f);
-
-        sumNumerator += pow(numerator * denominator, vec3(1.0 / fittingGamma));
-        sumDenominator += pow(denominator * denominator, vec3(1.0 / fittingGamma));
-    }
-
-    vec3 fresnel = PI * f0 * pow(sumNumerator / sumDenominator, vec3(fittingGamma));
-    Color = vec4(pow(fresnel, vec3(1.0 / gamma)), 1.0);
 }
--- src/tetzlaff/ibrelight/export/PTMfit/PTMReconstruction.java
+++ src/tetzlaff/ibrelight/export/PTMfit/PTMReconstruction.java
@@ -21,10 +21,6 @@ public class PTMReconstruction <ContextType extends Context<ContextType>>{
     private final int imageWidth;
     private final int imageHeight;
     public final Texture3D<ContextType> weightMaps;
-//    public final Texture2D<ContextType> weightMask;
-//    public final Texture2D<ContextType> basisMaps;
-//    public final UniformBuffer<ContextType> diffuseUniformBuffer;
-
 
 
     public PTMReconstruction(IBRResources<ContextType> resources,TextureFitSettings settings) {
@@ -49,24 +45,6 @@ public class PTMReconstruction <ContextType extends Context<ContextType>>{
                 .setLinearFilteringEnabled(true)
                 .setMipmapsEnabled(false)
                 .createTexture();
-
-//        weightMask = context.getTextureFactory().build2DColorTexture(settings.width, settings.height)
-//                .setInternalFormat(ColorFormat.R8)
-//                .setLinearFilteringEnabled(true)
-//                .setMipmapsEnabled(false)
-//                .createTexture();
-
-//        basisMaps = context.getTextureFactory().build1DColorTextureArray(
-//                91,8)
-//                .setInternalFormat(ColorFormat.RGB32F)
-//                .setLinearFilteringEnabled(true)
-//                .setMipmapsEnabled(false)
-//                .createTexture();
-//
-//        diffuseUniformBuffer = context.createUniformBuffer();
-//
-//        weightMaps.setTextureWrap(TextureWrapMode.None, TextureWrapMode.None, TextureWrapMode.None);
-//        basisMaps.setTextureWrap(TextureWrapMode.None, TextureWrapMode.None);
     }
 
     public void reconstruct(PTMsolution solutions,ProgramBuilder<ContextType> programBuilder, String directoryName){
@@ -81,10 +59,7 @@ public class PTMReconstruction <ContextType extends Context<ContextType>>{
                 program ->
                 {
                     resources.setupShaderProgram(program);
-//                    program.setTexture("basisFunctions", this.basisMaps);
                     program.setTexture("weightMaps", this.weightMaps);
-//                    program.setTexture("weightMask", this.weightMask);
-//                    program.setUniformBuffer("DiffuseColors", this.diffuseUniformBuffer);
                 }
                 )
 
@@ -95,15 +70,13 @@ public class PTMReconstruction <ContextType extends Context<ContextType>>{
             reconstruction.execute((k, framebuffer) -> saveReconstructionToFile(directoryName, k, framebuffer));
             NativeVectorBufferFactory factory = NativeVectorBufferFactory.getInstance();
             NativeVectorBuffer weightMapBuffer = factory.createEmpty(NativeDataType.FLOAT, 1, settings.width * settings.height);
-            NativeVectorBuffer basisMapBuffer = factory.createEmpty(NativeDataType.FLOAT, 3, 8 * (90 + 1));
-            NativeVectorBuffer diffuseNativeBuffer = factory.createEmpty(NativeDataType.FLOAT, 4, 8);
             for (int p = 0; p < settings.width * settings.height; p++)
             {
                 weightMapBuffer.set(p, 0, solutions.areWeightsValid(p) ? 1.0 : 0.0);
             }
-//            weightMask.load(weightMapBuffer);
 
-            for (int b = 0; b < 8; b++)
+
+            for (int b = 0; b < 6; b++)
             {
                 // Copy weights from the individual solutions into the weight buffer laid out in texture space to be sent to the GPU.
                 for (int p = 0; p < settings.width * settings.height; p++)
@@ -114,28 +87,7 @@ public class PTMReconstruction <ContextType extends Context<ContextType>>{
                 // Immediately load the weight map so that we can reuse the local memory buffer.
                 weightMaps.loadLayer(b, weightMapBuffer);
 
-                // Copy basis functions by color channel into the basis map buffer that will eventually be sent to the GPU..
-                for (int m = 0; m <= 90; m++)
-                {
-                    // Format necessary for OpenGL is essentially transposed from the storage in the solution vectors.
-                    basisMapBuffer.set(m + (90 + 1) * b, 0, new SimpleMatrix(91, 8, DMatrixRMaj.class).get(m, b));
-                    basisMapBuffer.set(m + (90 + 1) * b, 1, new SimpleMatrix(91, 8, DMatrixRMaj.class).get(m, b));
-                    basisMapBuffer.set(m + (90 + 1) * b, 2, new SimpleMatrix(91, 8, DMatrixRMaj.class).get(m, b));
-                }
-
-                // Store each channel of the diffuse albedo in the local buffer.
-                diffuseNativeBuffer.set(b, 0, solutions.getDiffuseAlbedo(b).x);
-                diffuseNativeBuffer.set(b, 1, solutions.getDiffuseAlbedo(b).y);
-                diffuseNativeBuffer.set(b, 2, solutions.getDiffuseAlbedo(b).z);
-                diffuseNativeBuffer.set(b, 3, 1.0f);
             }
-
-            // Send the basis functions to the GPU.
-//            basisMaps.load(basisMapBuffer);
-
-            // Send the diffuse albedos to the GPU.
-//            diffuseUniformBuffer.setData(diffuseNativeBuffer);
-
         }
         catch (FileNotFoundException e)
         {
