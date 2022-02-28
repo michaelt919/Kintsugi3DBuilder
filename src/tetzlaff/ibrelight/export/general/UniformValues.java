61a2e1245a71fb2f5a7e1d38fa8041c3625a27a1
mey-Amor <63292508+mey-Amor@users.noreply.github.com>
2022-02-28 15:06:30 -0600

    Javafx

---
+++ src/tetzlaff/ibrelight/export/general/UniformValues.java
@@ -0,0 +1,31 @@
+package tetzlaff.ibrelight.export.general;
+/*
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import tetzlaff.ibrelight.javafx.util.StaticUtilities;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UniformValues {
+
+    @FXML private TextField rangeDistance;
+    @FXML private TextField rangeAperture;
+    @FXML private TextField rangeFocalLength;
+
+    @FXML private Slider rangeSliderDistance;
+    @FXML private Slider rangeSliderAperture;
+    @FXML private Slider rangeSliderFocalLength;
+
+    private final DoubleProperty focalLength = new SimpleDoubleProperty();
+
+
+    @FXML private void rangeSliderDistanceAction() {
+
+        rangeDistance.setText(String.valueOf(rangeSliderDistance.getValue()));
+    }
+
+}*/
