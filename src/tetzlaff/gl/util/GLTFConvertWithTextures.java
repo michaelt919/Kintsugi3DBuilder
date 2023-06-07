4372012715739ee9b4ab9f4977ae72640a47188b
Jacob Buelow <jdbuelow@gmail.com>
2023-06-07 16:56:39 -0500

    Found a really hacky workaround to allow external URLs for images to be encoded in the exported glTF

--- src/tetzlaff/gl/util/GLTFConvertWithTextures.java
+++ src/tetzlaff/gl/util/GLTFConvertWithTextures.java
@@ -1,19 +1,24 @@
 package tetzlaff.gl.util;
 
+import de.javagl.jgltf.impl.v2.Image;
 import de.javagl.jgltf.model.GltfModel;
 import de.javagl.jgltf.model.MeshModel;
 import de.javagl.jgltf.model.MeshPrimitiveModel;
 import de.javagl.jgltf.model.creation.GltfModelBuilder;
 import de.javagl.jgltf.model.impl.*;
-import de.javagl.jgltf.model.io.GltfModelWriter;
+import de.javagl.jgltf.model.io.v2.GltfAssetV2;
+import de.javagl.jgltf.model.io.v2.GltfAssetWriterV2;
+import de.javagl.jgltf.model.io.v2.GltfAssetsV2;
 import de.javagl.jgltf.model.v2.MaterialModelV2;
 import de.javagl.jgltf.obj.model.ObjGltfModelCreator;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 
 /**
  * Runnable test to open a .obj file, convert it to glTF and add textures
@@ -61,6 +66,24 @@ public class GLTFConvertWithTextures {
         weights00Texture.setName("weights00");
         builder.addTextureModel(weights00Texture);
 
+        // So can we add a texture that is just a uri?
+        DefaultTextureModel urlTextureModel = new DefaultTextureModel();
+        DefaultImageModel urlImageModel = new DefaultImageModel();
+
+        // Trick the MIME-type detector into writing "image/url"
+        urlImageModel.setUri(".url");
+
+        // Something in the Gltf writer really wants the imageData field to not be null, throwing a NullPointerException without it
+        // Yet specifying an image data buffer overwrites the uri set above... de.javagl.jgltf.model.io.v2.EmbeddedAssetCreatorV2.convertImageToEmbedded
+
+        // Write the url into the image data field
+        ByteBuffer emptyByteBuffer = ByteBuffer.wrap("https://example.com/example-texture.png".getBytes());
+        urlImageModel.setImageData(emptyByteBuffer);
+
+        urlTextureModel.setImageModel(urlImageModel);
+
+        builder.addTextureModel(urlTextureModel);
+
         for (MeshModel mesh : gltfModel.getMeshModels()) {
 
             DefaultMeshModel meshModel = new DefaultMeshModel();
@@ -82,10 +105,26 @@ public class GLTFConvertWithTextures {
         GltfModel newModel = builder.build();
         System.out.println("Built converted glTF");
 
+        GltfAssetV2 gltfAsset = GltfAssetsV2.createEmbedded(newModel);
+
+        // For images that we've hacked external image urls onto from above, replace the uri field with the actual url
+        // instead of the base64 encoded url.
+        // Are we sure external URLs are supposed to be supported by glTF? Because they certainly aren't supported by jgltf.
+        for (Image img : gltfAsset.getGltf().getImages()) {
+            String url = img.getUri();
+            if (url.startsWith("data:image/url")) {
+                String urlb64 = url.replace("data:image/url;base64,", "");
+                String decodedUrl = new String(Base64.getDecoder().decode(urlb64));
+                img.setUri(decodedUrl);
+            }
+        }
+
         // Write glb file from converted model
-        GltfModelWriter gltfModelWriter = new GltfModelWriter();
+        //GltfModelWriter gltfModelWriter = new GltfModelWriter();
+        //gltfModelWriter.writeEmbedded(newModel, glbPath.toFile());
         Path glbPath = basePath.resolve("output/out.glb");
-        gltfModelWriter.writeBinary(newModel, glbPath.toFile());
+        GltfAssetWriterV2 gltfAssetWriter = new GltfAssetWriterV2();
+        gltfAssetWriter.writeBinary(gltfAsset, new FileOutputStream(glbPath.toFile()));
     }
 
     private static DefaultTextureModel loadTextureFromDisk(Path path) throws IOException {
