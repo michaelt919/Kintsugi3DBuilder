324bffe021181486a5a0b9933984c0d371df7e19
denneyl2711 <lukedenneymn@gmail.com>
2023-06-26 17:05:18 -0500

    Layered unzipping is finished. Program only outputs the chunk labels (no chunk selection yet).

--- src/tetzlaff/gl/util/UnzipHelper.java
+++ src/tetzlaff/gl/util/UnzipHelper.java
@@ -1,48 +1,50 @@
 package tetzlaff.gl.util;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class UnzipHelper {
-    public UnzipHelper() {
+    private UnzipHelper() {
     }
 
-    public void unzip() throws IOException {
-        //TODO: BOTH NEED TO BE SELECTABLE BY USER
-        String zipFileName = "C:\\Users\\DenneyLuke\\OneDrive - University of Wisconsin-Stout\\Summer 2023\\unzipping.zip";
-        String destDirectory = "C:\\Users\\DenneyLuke\\OneDrive - University of Wisconsin-Stout\\Summer 2023\\unzipDestination";
-
-        File destDirectoryFolder = new File(destDirectory);
-        if (!destDirectoryFolder.exists()) {
-            destDirectoryFolder.mkdir();
-        }
-        byte[] buffer = new byte[1024];
+    public static String unzipToString(String zipFileName) throws IOException {
+        //unzip the zip file (which should be a .psx) and return the contents as a string
+        //intended to only unzip one file
+        //string will be converted to XML outside of this function
+        //TODO: NEED TO ADD SAFETY FOR WHEN IT OPENS A ZIP WITH MULTIPLE FILES?
+        //TODO: SONARLINT UPSET BECAUSE THE ZIPINPUTSTREAM MAY NOT BE CLOSED (if an exception occurs)
         ZipInputStream zis= new ZipInputStream(new FileInputStream(zipFileName));
-        ZipEntry zipEntry = zis.getNextEntry();
-        while(zipEntry !=null) {
-            String filePath = destDirectory + File.separator + zipEntry.getName();
-            System.out.println("Unzipping "+filePath);
-            if(!zipEntry.isDirectory()) {
-                FileOutputStream fos = new FileOutputStream(filePath);
-                int len;
-                while ((len = zis.read(buffer)) >0){
-                    fos.write(buffer,0,len);
-                }
-                fos.close();
+        byte[] buffer = new byte[1024];
+        StringBuilder s = new StringBuilder();
+        int read = 0;
+        while ((zis.getNextEntry())!= null) {
+            while ((read = zis.read(buffer, 0, 1024)) >= 0) {
+                s.append(new String(buffer, 0, read));
             }
-            else {
-                File dir = new File(filePath);
-                dir.mkdir();
-            }
-            zis.closeEntry();
-            zipEntry = zis.getNextEntry();
         }
         zis.closeEntry();
         zis.close();
-        System.out.println("Unzipping complete");
+        return s.toString();
     }
+
+    public static Document convertStringToDocument(String xmlStr) {
+        //Taken from https://www.digitalocean.com/community/tutorials/java-convert-string-to-xml-document-and-xml-document-to-string
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        try {
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new InputSource(new StringReader(xmlStr)));
+            return doc;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
--- src/tetzlaff/ibrelight/javafx/controllers/menubar/UnzipFileSelectionController.java
+++ src/tetzlaff/ibrelight/javafx/controllers/menubar/UnzipFileSelectionController.java
@@ -4,10 +4,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import tetzlaff.gl.util.UnzipHelper;
 
+import javax.tools.Tool;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 
 public class UnzipFileSelectionController {
     @FXML
@@ -16,10 +29,14 @@ public class UnzipFileSelectionController {
     private File selectedFile;
     private Stage stage;
 
+    private DirectoryChooser directoryChooser;
+
     @FXML
     public TextField psxPathTxtField;
-
+    @FXML
+    private TextField outputDirectoryPathTxtField;
     public void init(){
+        this.directoryChooser = new DirectoryChooser();
     }
 
     public void selectPSX(ActionEvent actionEvent) {
@@ -35,8 +52,68 @@ public class UnzipFileSelectionController {
         }
     }
 
-    public void unzip(ActionEvent actionEvent) {
-        //TODO: IMPLEMENT THIS
+    public void parseXML(ActionEvent actionEvent) {
+        //TODO: NEED TO ADD NULL SAFETY AND GENERAL CLEANUP/USER INPUT PROTECTION
+        //open .psx as an XML file and grab the path attribute from the document tag
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        //TODO: SONARLIST IS COMPLAINING ABOUT XXE ATTACKS
+
+        try{
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            String psxFilePath = psxPathTxtField.getText();
+
+            //Get Document from .psx file
+            Document document = builder.parse(new File(psxFilePath));
+
+            //get the path attribute from the document tag
+            NodeList nodes = document.getElementsByTagName("document");
+            Element documentTag = (Element) nodes.item(0);
+
+            //this gives "{projectname}.files/project.zip"
+            //need to replace {projectname} with full path (except .psx)
+            String documentPathInfo = documentTag.getAttribute("path");
+
+            documentPathInfo = documentPathInfo.substring(14);
+            documentPathInfo = psxFilePath.substring(0, psxFilePath.length() - 3) + documentPathInfo;
+
+            //extract project.zip and open the doc.xml
+            String projectZipString = UnzipHelper.unzipToString(documentPathInfo);
+            Document docXML = UnzipHelper.convertStringToDocument(projectZipString);
+
+            //find the chunks and open the .zip for each chunk
+            NodeList chunkList = docXML.getElementsByTagName("chunk");
+            String chunkZipPath;
+            for(int i = 0; i < chunkList.getLength(); ++i) {
+                Node chunk = chunkList.item(i);
+
+                if (chunk.getNodeType() == Node.ELEMENT_NODE) {
+                    Element chunkElement = (Element) chunk;
+
+                    //open doc.xml within each chunk and read the chunk's label attribute --> display it to user
+                    chunkZipPath = chunkElement.getAttribute("path"); //gives xx/chunk.zip where xx is a number
+                    chunkZipPath = psxFilePath.substring(0, psxFilePath.length() - 4) + ".files\\" + chunkZipPath;//append this path to the psxFilePath (without that path's .psx)
+
+                    Document chunkDocument = UnzipHelper.convertStringToDocument(UnzipHelper.unzipToString(chunkZipPath));//path --> String --> XML document
+                    chunkDocument.getDocumentElement().normalize();
+
+                    NodeList innerChunkList = chunkDocument.getElementsByTagName("chunk");
+                    for(int j = 0; j < innerChunkList.getLength(); ++j) {
+                        Node innerChunk = innerChunkList.item(j);
+
+                        if (innerChunk.getNodeType() == Node.ELEMENT_NODE) {
+                            Element innerChunkElement = (Element) innerChunk;
+                            System.out.println(innerChunkElement.getAttribute("label"));
+                        }
+                    }
+                }
+                //user selects a chunk to load --> treat that chunk's doc.xml as the camera's xml file
+                //TODO: IMPLEMENT CHUNK SELECTION
+            }
+
+        } catch (ParserConfigurationException|IOException|SAXException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private boolean loadPSXFromTxtField() {
@@ -50,6 +127,21 @@ public class UnzipFileSelectionController {
         }
         else{
             return false;//file does not exist
+        }
+    }
+
+    public void selectOutputDirectory(ActionEvent actionEvent) {
+        this.directoryChooser.setTitle("Choose an output directory");
+
+        stage = (Stage) outputDirectoryPathTxtField.getScene().getWindow();
+        File file = this.directoryChooser.showDialog(stage.getOwner());
+//TODO: NULL POINTER EXCEPTION IF THE USER CANCELS THE REQUEST TO SPECIFY A DIRECTORY
+        if (file.exists()){
+            directoryChooser.setInitialDirectory(file);
+            outputDirectoryPathTxtField.setText(file.getAbsolutePath());
+        }
+        else{
+            Toolkit.getDefaultToolkit().beep();
         }
     }
 }
