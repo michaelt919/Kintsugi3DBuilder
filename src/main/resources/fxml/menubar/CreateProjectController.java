1c7eb109e76e9c7dbe063d5478446643d61c5cb4
denneyl2711 <lukedenneymn@gmail.com>
2023-07-21 13:59:11 -0500

    Removed unneeded settings from create module.

--- src/main/resources/fxml/menubar/CreateProjectController.java
+++ src/main/resources/fxml/menubar/CreateProjectController.java
@@ -4,13 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-import javax.swing.*;
 import java.awt.Toolkit;
 import java.io.File;
 
@@ -20,21 +18,10 @@ public class CreateProjectController {
     @FXML public TextField projectNameTxtField;
     @FXML public ChoiceBox<String> directoryChoices;
 
-    @FXML public CheckBox imageCompressCheckbox;
     @FXML public CheckBox import3DOriginCheckbox;
-    @FXML public CheckBox visibilityAndShadowTestingCheckbox;
-
     @FXML public CheckBox spatialOrientation3DOriginCheckbox;
     @FXML public CheckBox importedTransparencyCheckbox;
-    @FXML public CheckBox mipmapCheckbox;
     @FXML public CheckBox autosaveCheckbox;
-
-    @FXML public TextField widthTextField;
-    @FXML public TextField heightTextField;
-
-    private TextField[] intTxtFields;
-    @FXML public Label widthLabel;
-    @FXML public Label heightLabel;
 
     private Stage stage;
 
@@ -53,11 +40,6 @@ public class CreateProjectController {
 
         //attach event handler (this cannot be done in scenebuilder)
         directoryChoices.setOnAction(this::handleDirectoryDropdownSelection);
-
-        //disable the "Preload Visibility & Shadow Testing" section if the checkbox is not selected
-        updateVisibility();
-
-        intTxtFields  = new TextField[]{widthTextField, heightTextField};
 
         stage = (Stage) directoryChoices.getScene().getWindow();
     }
@@ -92,17 +74,10 @@ public class CreateProjectController {
         System.out.println("Project Name: " + projectNameTxtField.getText());
         System.out.println("Project Location: " + directoryChoices.getValue());
 
-        System.out.println("Image compression? --> " + imageCompressCheckbox.isSelected());
         System.out.println("Use imported 3D origin? --> " + import3DOriginCheckbox.isSelected());
         System.out.println("Spatial orientation origin point as 3D origin? --> " + spatialOrientation3DOriginCheckbox.isSelected());
 
-        System.out.println("Preload visibility and testing? --> " + visibilityAndShadowTestingCheckbox.isSelected());
-        System.out.println("Valid int fields? --> " + areIntFieldsValid());
-        System.out.println("Width: " + widthTextField.getText());
-        System.out.println("Height: " + heightTextField.getText());
-
         System.out.println("Import transparency? --> " + importedTransparencyCheckbox.isSelected());
-        System.out.println("Mipmap? --> " + mipmapCheckbox.isSelected());
 
         System.out.println("Autosave? --> " + autosaveCheckbox.isSelected());
 
@@ -111,26 +86,16 @@ public class CreateProjectController {
 
     }
 
-    public void updateVisibility() {//TODO: NEEDS A RENAME
-        //disable all options in Preload Visibility & Shadow Testing section if the corresponding checkbox is disabled
-        boolean isCheckmarked = visibilityAndShadowTestingCheckbox.isSelected();
-        widthTextField.setDisable(!isCheckmarked);
-        heightTextField.setDisable(!isCheckmarked);
-
-        widthLabel.setDisable(!isCheckmarked);
-        heightLabel.setDisable(!isCheckmarked);
-    }
-
-    private boolean areIntFieldsValid(){
-        for(TextField txtField : intTxtFields) {
-            if (txtField == null ||
-                    txtField.getText() == null ||
-                    !txtField.getText().matches("-?\\d+")) {//regex to check if input is integer
-                return false;
-            }
-        }
-        return true;
-    }
+//    private boolean areIntFieldsValid(){
+//        for(TextField txtField : intTxtFields) {
+//            if (txtField == null ||
+//                    txtField.getText() == null ||
+//                    !txtField.getText().matches("-?\\d+")) {//regex to check if input is integer
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
     @FXML
     public void cancelButtonAction()
