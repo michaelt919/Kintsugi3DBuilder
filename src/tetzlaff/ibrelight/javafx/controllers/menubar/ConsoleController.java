6dcdac7df9a626c41ee7be9c360483cdb9e57be0
Jacob Buelow <jdbuelow@gmail.com>
2023-07-26 14:27:43 -0500

    Add log directory button, and switch to a cell factory

--- src/tetzlaff/ibrelight/javafx/controllers/menubar/ConsoleController.java
+++ src/tetzlaff/ibrelight/javafx/controllers/menubar/ConsoleController.java
@@ -1,14 +1,21 @@
 package tetzlaff.ibrelight.javafx.controllers.menubar;
 
+import javafx.application.HostServices;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.util.Callback;
 import org.apache.logging.log4j.core.LogEvent;
+import tetzlaff.ibrelight.app.IBRelight;
 import tetzlaff.ibrelight.app.logging.LogMessage;
 import tetzlaff.ibrelight.app.logging.LogMessageListener;
 import tetzlaff.ibrelight.app.logging.RecentLogMessageAppender;
+import tetzlaff.ibrelight.javafx.MainApplication;
 
 import java.net.URL;
 import java.util.List;
@@ -21,7 +28,7 @@ public class ConsoleController implements Initializable
     @FXML private CheckBox checkboxDebug;
     @FXML private CheckBox checkboxTrace;
     @FXML private CheckBox checkboxFatal;
-    @FXML private ListView<String> messageListView;
+    @FXML private ListView<LogMessage> messageListView;
 
     private RecentLogMessageAppender logMessages;
 
@@ -29,6 +36,8 @@ public class ConsoleController implements Initializable
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         logMessages = RecentLogMessageAppender.getInstance();
+
+        messageListView.setCellFactory(new LogMessageCellFactory());
 
         populateListFromCache();
 
@@ -38,7 +47,7 @@ public class ConsoleController implements Initializable
             public void newLogMessage(LogMessage logMessage)
             {
                 Platform.runLater(() -> {
-                    messageListView.getItems().add(logMessage.getMessage());
+                    messageListView.getItems().add(logMessage);
                 });
             }
         });
@@ -50,7 +59,35 @@ public class ConsoleController implements Initializable
 
         for (LogMessage msg : logMessages.getMessages())
         {
-            messageListView.getItems().add(msg.getMessage());
+            messageListView.getItems().add(msg);
+        }
+    }
+
+    public void buttonOpenLogDir(ActionEvent actionEvent)
+    {
+        //TODO
+    }
+
+    private class LogMessageCellFactory implements Callback<ListView<LogMessage>, ListCell<LogMessage>>
+    {
+        @Override
+        public ListCell<LogMessage> call(ListView<LogMessage> logMessageListView)
+        {
+            return new ListCell<>()
+            {
+                @Override
+                public void updateItem(LogMessage message, boolean empty)
+                {
+                    super.updateItem(message, empty);
+                    if (empty || message == null)
+                    {
+                        setText(null);
+                    } else
+                    {
+                        setText(message.getMessage());
+                    }
+                }
+            };
         }
     }
 }
