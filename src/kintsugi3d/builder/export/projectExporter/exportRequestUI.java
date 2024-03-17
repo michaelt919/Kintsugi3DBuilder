2f9960b611e9ed9660dff9c020122992c2bef21e
Isaac-tesch <99272840+Isaac-tesch@users.noreply.github.com>
2024-03-17 14:57:01 -0500

    Switched form textField to a comboBox
    Swtiched Minimum texture res. to a comboBox instead of a textField. Cleaned up spots of code and added comments.

--- src/kintsugi3d/builder/export/projectExporter/exportRequestUI.java
+++ src/kintsugi3d/builder/export/projectExporter/exportRequestUI.java
@@ -1,18 +1,19 @@
 package kintsugi3d.builder.export.projectExporter;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-import kintsugi3d.builder.core.IBRRequestQueue;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import kintsugi3d.builder.core.IBRRequestUI;
+import kintsugi3d.builder.core.IBRRequestQueue;
 import kintsugi3d.builder.core.Kintsugi3DBuilderState;
 import kintsugi3d.builder.fit.settings.ExportSettings;
 
@@ -21,22 +22,20 @@ import java.io.IOException;
 import java.net.URL;
 import kintsugi3d.gl.core.Context;
 
+
 public class exportRequestUI implements IBRRequestUI {
-
-    private Kintsugi3DBuilderState modelAccess;
+    //Initialize all the variables in the FXML file
+    @FXML public Kintsugi3DBuilderState modelAccess;
     @FXML private Stage stage;
-
-    //Initalizes all the variables in the FXML file
     @FXML private Button runButton;
     @FXML private CheckBox combineWeightsCheckBox;
     @FXML private CheckBox generateLowResolutionCheckBox;
     @FXML private CheckBox glTFEnabledCheckBox;
     @FXML private CheckBox glTFPackTexturesCheckBox;
     @FXML private CheckBox openViewerOnceCheckBox;
-    @FXML private TextField minimumTextureResolutionTextField;
+    @FXML private ComboBox<Integer> minimumTextureResolutionComboBox;
 
-
-    //ComboBox<Boolean>[] boxArray = {combineWeightsBox, generateLowResTexturesBox, glTFEnabledBox, glTFPackTexturesBox, openViewerOnceCompleteBox};
+    
     public static exportRequestUI create(Window window, Kintsugi3DBuilderState modelAccess) throws IOException {
         String fxmlFileName = "fxml/export/ExportRequestUI.fxml";
         URL url = exportRequestUI.class.getClassLoader().getResource(fxmlFileName);
@@ -56,38 +55,37 @@ public class exportRequestUI implements IBRRequestUI {
     }
 
     @Override
-    public <ContextType extends Context<ContextType>> void prompt(IBRRequestQueue<ContextType> requestQueue){
+    public <ContextType extends Context<ContextType>> void prompt(IBRRequestQueue<ContextType> requestQueue) {
         ExportSettings settings = new ExportSettings();
 
         stage.show();
 
-        //Sets all the values to what they are in settings
+        //Sets all the values to the default
         combineWeightsCheckBox.setSelected(settings.isCombineWeights());
         generateLowResolutionCheckBox.setSelected(settings.isGenerateLowResTextures());
         glTFEnabledCheckBox.setSelected(settings.isGlTFEnabled());
         glTFPackTexturesCheckBox.setSelected(settings.isGlTFPackTextures());
         openViewerOnceCheckBox.setSelected(settings.isOpenViewerOnceComplete());
         int getMinimumTexRes = settings.getMinimumTextureResolution();
-        minimumTextureResolutionTextField.setText(Integer.toString(getMinimumTexRes));
+        minimumTextureResolutionComboBox.setItems(FXCollections.observableArrayList(32,64,128,256));
+        minimumTextureResolutionComboBox.setValue(getMinimumTexRes);
 
+        //Just sets the values in settings doesn't do anything else yet
         runButton.setOnAction(event ->
         {
-            //Once the function is ran they set the settings to the new selected values
+            //Once the function is run they set the settings to the new selected values
             settings.setCombineWeights(combineWeightsCheckBox.isSelected());
             settings.setGenerateLowResTextures(generateLowResolutionCheckBox.isSelected());
             settings.setGlTFEnabled(glTFEnabledCheckBox.isSelected());
             settings.setGlTFPackTextures(glTFPackTexturesCheckBox.isSelected());
             settings.setOpenViewerOnceComplete(openViewerOnceCheckBox.isSelected());
-
-            int minimumTextureRes = Integer.parseInt(minimumTextureResolutionTextField.getText());
-            settings.setMinimumTextureResolution(minimumTextureRes);
-
+            settings.setMinimumTextureResolution(minimumTextureResolutionComboBox.getValue());
+            System.out.println(minimumTextureResolutionComboBox.getValue());
         });
     }
 
-    @FXML
-    public void cancelButtonAction()
-    {
+    @FXML //closes the stage
+    public void cancelButtonAction() {
         stage.close();
     }
 }
