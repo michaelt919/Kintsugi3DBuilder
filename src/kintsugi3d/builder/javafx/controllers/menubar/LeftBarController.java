7374db6d503dafebfa909012b9aad0d226817fb3
Jacob <jacobdsletten@gmail.com>
2025-06-04 14:48:36 -0500

    Imported LeftBar Files

---
+++ src/kintsugi3d/builder/javafx/controllers/menubar/LeftBarController.java
@@ -0,0 +1,30 @@
+package kintsugi3d.builder.javafx.controllers.menubar;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class LeftBarController {
+
+    @FXML
+    public RadioButton TexturesButton, CamerasButton;
+
+    @FXML
+    public VBox textureTab, cameraTab;
+
+    @FXML
+    private void chooseTabAction(ActionEvent event) throws IOException {
+        textureTab.setVisible(TexturesButton.isSelected());
+        textureTab.setManaged(TexturesButton.isSelected());
+        cameraTab.setVisible(CamerasButton.isSelected());
+        cameraTab.setManaged(CamerasButton.isSelected());
+
+        System.out.println("TextureTab Visible: " + textureTab.isVisible());
+        System.out.println("CameraTab Visible: " + cameraTab.isVisible());
+
+    }
+
+}
