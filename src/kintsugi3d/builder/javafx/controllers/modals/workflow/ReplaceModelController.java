40d4d7bc8fffddcea013e57e9636836c4ac881d9
JoeLuther13 <lutherj1002@my.uwstout.edu>
2026-07-02 14:36:44 -0500

    Finished styling work for refresh and replace.

--- src/kintsugi3d/builder/javafx/controllers/modals/workflow/ReplaceModelController.java
+++ src/kintsugi3d/builder/javafx/controllers/modals/workflow/ReplaceModelController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.stage.FileChooser;
